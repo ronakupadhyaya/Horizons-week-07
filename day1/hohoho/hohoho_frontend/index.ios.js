2763100b85a562ad57679458f4f5fa6065843b12
Lane Rettig <lanerettig@gmail.com>
2016-07-10 22:19:41 -0400

    remove ES6 syntax, get first couple of basic routes working

--- day1/hohoho/hohoho_frontend/index.ios.js
+++ day1/hohoho/hohoho_frontend/index.ios.js
@@ -1,53 +1,93 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+const React = require('react');
+const AppRegistry = require('react-native').AppRegistry;
+const Navigator = require('react-native').Navigator;
+const Text = require('react-native').Text;
+const View = require('react-native').View;
+const TouchableOpacity = require('react-native').TouchableOpacity;
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+// This is the root view
+const hohoho_frontend = React.createClass({
+  render: function() {
+    return (
+      <Navigator
+        initialRoute={{ index: 0 }}
+        renderScene={function(route, navigator) {
+          // Function to call when a new scene should be displayed
+          function onForward() {
+            navigator.push({
+              index: route.index + 1,
+            });
+          }
 
-class hohoho_frontend extends Component {
-  render() {
+          // Function to call to go back to the previous scene
+          function onBack() {
+            if (route.index > 0) {
+              navigator.pop();
+            }
+          }
+
+          switch (route.index) {
+            case 0:
+              return (
+                <LoginScreen
+                  onForward={onForward}
+                  onBack={onBack}
+                />
+              );
+            case 1:
+              return (
+                <MainScreen
+                  onForward={onForward}
+                  onBack={onBack}
+                />
+              );
+            default:
+              throw new Error("Unknown route");
+          }
+        }}
+      />
+    );
+  }
+});
+
+const LoginScreen = React.createClass({
+  render: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <Text style={styles.textBig}>Login screen</Text>
+        <TouchableOpacity onPress={this.props.onForward}>
+          <Text>Tap to Login</Text>
+        </TouchableOpacity>
       </View>
     );
   }
-}
+});
 
-const styles = StyleSheet.create({
+const MainScreen = React.createClass({
+  render: function() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.textBig}>Main app</Text>
+        <TouchableOpacity onPress={this.props.onBack}>
+          <Text>Tap to go back to Login Screen</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+});
+
+const styles = {
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
+  textBig: {
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  }
+};
 
-AppRegistry.registerComponent('hohoho_frontend', () => hohoho_frontend);
+AppRegistry.registerComponent('hohoho_frontend', function() { return hohoho_frontend });
