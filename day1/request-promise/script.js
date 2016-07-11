62ed11db2a13689418ff5ad4870b5a7c510e3a5d
Isabella Gong <isabella.gong@gmail.com>
2016-07-11 14:41:41 -0400

    finished

--- day1/request-promise/script.js
+++ day1/request-promise/script.js
@@ -16,9 +16,24 @@ request.get(url, {json: true})
   .then(function(resp) {
     // Response from the second request
     console.log('Success 2:', resp);
+    return request.get(url, {json: true, qs: { key: resp.key, stage: resp.stage + 1 }});
   })
 // YOUR CODE HERE
-
+	.then(function(resp) {
+    // qs: represents the query parameters
+    // Note how we need to return the Promise we get back from request.get()
+    return request.get(url, {json: true, qs: { key: resp.key, stage: resp.stage + 1 }});
+  })
+	.then(function(resp) {
+    // qs: represents the query parameters
+    // Note how we need to return the Promise we get back from request.get()
+    return request.get(url, {json: true, qs: { key: resp.key, stage: resp.stage + 1 }});
+  })
+	.then(function(resp) {
+    // qs: represents the query parameters
+    // Note how we need to return the Promise we get back from request.get()
+    console.log('Success:', resp);
+  })
 // Add code to fetch the next 3 stages of this exercise
 // When you're done, you'll see the response:
 // {"success":true,"completed":true,"reason":"Congratulations! You've completed this exercise."}
