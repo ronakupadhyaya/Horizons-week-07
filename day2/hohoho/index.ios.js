567dceb554529e22145868315a08c03b03c05a96
Isabella Gong <isabella.gong@gmail.com>
2016-07-13 19:25:52 -0400

    FINISHED

--- day2/hohoho/index.ios.js
+++ day2/hohoho/index.ios.js
@@ -3,12 +3,15 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
+  Alert,
   View,
   TouchableOpacity,
   TextInput,
   NavigatorIOS,
   ListView,
-  Dimensions
+  Dimensions,
+  AsyncStorage,
+  MapView
 } from 'react-native'
 
 var height = Dimensions.get('window').height;
@@ -85,9 +88,44 @@ var Register = React.createClass({
 
 var Login = React.createClass({
   getInitialState() {
-    return {message: null}
+    return {message:null}
   },
+  componentDidMount() {
+    AsyncStorage.getItem('user')
+    .then(result => {
+      var parsedResult = JSON.parse(result);
+      console.log('result', result)
+      var username = parsedResult.username;
+      var password = parsedResult.password;
+      if (username && password) {
+        this.setState(
+        {
+          username, password
+        })
+        return {username, password}
+        
+      }
+    })
+    .then(this.press)
+    // Don't really need an else clause, we don't do anything in this case.
+    .catch(err => { /* handle the error */ 
+      console.log("here");
+      console.log(err);
+    })
+  },
+  messages() {
+    this.props.navigator.push({
+      component: Messages,
+      title: 'Messages'
+    }
+
+    )
+  },
+
   press() {
+    console.log('attempting press');
+    console.log(this.state.username, "user")
+    console.log(this.state.password, "password")
     fetch('https://hohoho-backend.herokuapp.com/login', {
       method: 'POST',
       headers: {
@@ -103,9 +141,15 @@ var Login = React.createClass({
       console.log(responseJson);
       console.log(responseJson.success);
       if (responseJson.success) {
+        AsyncStorage.setItem('user', JSON.stringify({
+          username: this.state.username,
+          password: this.state.password
+        }))
         this.props.navigator.push({
           component: Users,
-          title: "Users"
+          title: "Users",
+          rightButtonTitle: 'Messages',
+          onRightButtonPress: this.messages
         })
       }
 
@@ -175,31 +219,182 @@ var Users =  React.createClass({
     .then((responseJson) => {
       console.log(responseJson);
       console.log(responseJson.success);
-       
+
       if (responseJson.success) {
-       
-      this.setState({
-           dataSource: ds.cloneWithRows(responseJson.users)
-         })
+
+        this.setState({
+         dataSource: ds.cloneWithRows(responseJson.users)
+       })
       }
-  });
+    });
     return {
-         dataSource: ds.cloneWithRows([])
+     dataSource: ds.cloneWithRows([])
+   }
+ },
+ touchUser(user) {
+  fetch('https://hohoho-backend.herokuapp.com/messages', {
+
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      to: user._id
+    })
+  }).then((response) => response.json())
+  .then((responseJson) => {
+
+    if (responseJson.success) {
+      Alert.alert(
+        'Success',
+        'Your Ho Ho Ho! to' + user.username + 'has been sent!',
+          [{text: 'Close'}] // Button
+          )
+    }
+
+    else {
+      Alert.alert(
+        'Failure',
+        'Your Ho Ho Ho! to' + user.username + 'could not be sent!',
+          [{text: 'Close'}] // Button
+          )
+    }
+
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+},
+sendLocation(user) {
+  navigator.geolocation.getCurrentPosition(
+    position => {
+      console.log("Got position:", position);
+      /* use fetch() with the received position here */
+      fetch('https://hohoho-backend.herokuapp.com/messages', {
+      method: 'POST',
+      body: JSON.stringify({
+          to: user._id,
+          location: {
+            longitude: position.coords.longitude,
+            latitude: position.coords.latitude
+          }
+      }),
+      headers: {
+        "Content-Type": "application/json"
       }
-  },
-  render() {
-    return (
-      <View style={styles.container}>
-      <ListView style={{width: width*.8}}
-      dataSource={this.state.dataSource}
-      renderRow={(rowData) => <Text>{rowData.username}</Text>}
+    })
+      .then((response) => response.json())
+    .then((responseJson) => { 
+      if (responseJson.success) {
+        Alert.alert(
+          'Success',
+          'Sent location Ho ho ho!',
+          [{text: 'Cool'}]
+          )
+      }
+      else {
+        Alert.alert(
+          'Failure',
+          'Failed to send location Ho ho ho!',
+          [{text: 'Sorry'}]
+          )
+      }
+
+  }).catch(err => {
+      console.log(err)
+      })
+    },
+    error => alert(error.message),
+    {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+
+},
+render() {
+  return (
+    <View style={styles.container}>
+    <ListView style={{width: width*.8}}
+    dataSource={this.state.dataSource}
+    renderRow={(rowData) => <TouchableOpacity onPress={this.touchUser.bind(this, rowData)}
+    onLongPress={this.sendLocation.bind(this, rowData)}
+    delayLongPress={3000}>
+    <Text>{rowData.username}</Text></TouchableOpacity>}
+    />
+    </View>
+    )
+
+}
+
+})
+
+var Messages = React.createClass({
+  getInitialState() {
+   const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+   fetch('https://hohoho-backend.herokuapp.com/messages', {
+
+    method: 'GET',
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+   .then((response) => response.json())
+   .then((responseJson) => {
+    if(responseJson.success) {
+      this.setState({
+        dataSource: ds.cloneWithRows(responseJson.messages)
+      })
+    }
+
+  })
+   .catch((error) => {
+    console.error(error);
+  });
+
+   return {
+     dataSource: ds.cloneWithRows([])
+   };
+ },
+ render() {
+ 
+  return (
+    <View style={styles.container}>
+    <ListView style={{width: width*.8}}
+    dataSource={this.state.dataSource}
+    renderRow={(rowData) => <TouchableOpacity>
+      <Text>From: {rowData.from.username} {'\n'} 
+      To: {rowData.to.username} {'\n'} 
+      {rowData.timestamp} {'\n'} 
+      </Text>
+      {
+         (rowData.location && rowData.location.longitude) ? 
+          <View><Text>Hi</Text><MapView
+  style={{height: 60}}
+  showsUserLocation={true}
+  scrollEnabled={false}
+  region={{
+    longitude: rowData.location.longitude,
+    latitude: rowData.location.latitude,
+    longitudeDelta: 1,
+    latitudeDelta: 1
+  }}
+  annotations={[{
+    longitude: rowData.location.longitude, 
+    latitude: rowData.location.latitude, 
+    title: "Ethan's School"
+  }]}
+/></View> : null }
+      </TouchableOpacity>
+
+      }
+    
+     
+
       />
       </View>
       )
 
-  }
-
+}
 })
+
 const styles = StyleSheet.create({
   container: {
     flex: .8,
