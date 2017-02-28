20d3d3c9fa29706d0b63cb09fffd15da1a885d9f
Eric Abenojar <abenoeric@gmail.com>
2017-02-27 23:42:34 -0800

    reactnative

--- day1/hohoho/index.ios.js
+++ day1/hohoho/index.ios.js
@@ -7,7 +7,10 @@ import {
   TouchableOpacity,
   TextInput,
   NavigatorIOS,
-  ListView
+  ListView,
+  Button,
+  TextField,
+  Alert
 } from 'react-native'
 
 // This is the root view
@@ -25,19 +28,301 @@ var hohoho = React.createClass({
   }
 });
 
+var Users = React.createClass({
+  getInitialState(){
+    const ds = new ListView.DataSource({rowHasChanged: (r1,r2) => r1 !== r2});
+    return {
+      dataSource: ds.cloneWithRows([])
+    };
+  },
+  componentDidMount(){
+    var self = this;
+    fetch('https://hohoho-backend.herokuapp.com/users',{
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+    .then((resp) => resp.json())
+    .then((respJson)=>{
+      if(respJson.success){
+        console.log('Users should be displayed')
+        const ds = new ListView.DataSource({rowHasChanged: (r1,r2)=> r1 !== r2});
+        self.setState({
+          dataSource: ds.cloneWithRows(respJson.users)
+        })
+        console.log(respJson.users)
+      } else {
+        console.log('Error')
+      }
+
+
+    })
+    .catch((err)=>{
+      console.error(err)
+    });
+  },
+  press(user){
+    var self = this;
+    fetch('https://hohoho-backend.herokuapp.com/messages',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        to: user._id
+      })
+    })
+    .then((resp) => resp.json())
+    .then((respJson)=>{
+      if(respJson.success){
+        Alert.alert(
+          'Successssss',
+          'Your HoHoHo' + user.username + 'has been sent',
+           'has been sent',
+          [{text: 'HoHoHo'}]
+        )
+      } else {
+        Alert.alert(
+          'Faileddddd',
+          'Failed to send to message to ' + user.username,
+          [{text: 'AYYYEEYEYEYE'}]
+        )
+      }
+    })
+    .catch((err)=>{
+      console.error(err)
+    });
+  },
+  render(){
+    return(
+      <View style={styles.container}>
+        <ListView
+          dataSource={this.state.dataSource}
+
+          renderRow={(rowData)=>  <TouchableOpacity onPress={this.press.bind(this,rowData)}>
+          <Text style={{textAlign: 'center'}}>
+          {rowData.username}</Text>
+          </TouchableOpacity>
+        }
+        />
+      </View>
+    )
+  }
+});
+
+
+var Messages = React.createClass({
+  getInitialState(){
+    const ds = new ListView.DataSource({rowHasChanged: (r1,r2) => r1 !== r2});
+    return {
+      dataSource: ds.cloneWithRows([])
+    };
+  },
+  componentDidMount(){
+    var self = this;
+    fetch('https://hohoho-backend.herokuapp.com/messages',{
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+    .then((resp) => resp.json())
+    .then((respJson)=>{
+      if(respJson.success){
+        console.log('Users should be displayed')
+        const ds = new ListView.DataSource({rowHasChanged: (r1,r2)=> r1 !== r2});
+        self.setState({
+          dataSource: ds.cloneWithRows(respJson.messages)
+        })
+        console.log(respJson.users)
+      } else {
+        console.log('Error')
+      }
+
+
+    })
+    .catch((err)=>{
+      console.error(err)
+    });
+  },
+  render(){
+    return(
+      <View style={styles.container}>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData)=>  <View style={styles.container}>
+          <Text>From: {rowData.from.username}</Text>
+          <Text>To: {rowData.to.username}</Text>
+          <Text>Time: {rowData.timestamp}</Text>
+          </View>
+        }
+        />
+      </View>
+    )
+  }
+})
+
 var Register = React.createClass({
+  getInitialState(){
+    return {
+      username: '',
+      password: ''
+    }
+  },
+  main(){
+    fetch('https://hohoho-backend.herokuapp.com/register',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password
+      })
+    })
+    .then((resp) => resp.json())
+    .then((respJson)=>{
+      console.log(respJson, 'Just registered a user')
+      this.props.navigator.push({
+        component: Login,
+        title: "Login"
+      });
+
+    })
+    .catch((err)=>{
+      console.error(err)
+    });
+
+  },
   render() {
     return (
-      <View style={styles.container}>
+      <View style={styles.container} >
         <Text style={styles.textBig}>Register</Text>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Enter your username"
+          onChangeText={(text) => this.setState({username: text})}
+          value={this.state.username}
+        />
+        <TextInput
+          style={{height: 40}}
+          secureTextEntry={true}
+          placeholder="Enter your password"
+          onChangeText={(text) => this.setState({password: text})}
+          value={this.state.password}
+        />
+        <TouchableOpacity>
+          <Text style={{backgroundColor: 'red', color: 'white', width: 100 }} onPress={this.main}>Register</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 });
 
+var LoginInput = React.createClass({
+  getInitialState(){
+    return {
+      username: '',
+      password: '',
+      message: '',
+      users: []
+    }
+  },
+  messages(){
+    this.props.navigator.push({
+      component: Messages,
+      title: 'MessagePage'
+    })
+  },
+  main(){
+    fetch('https://hohoho-backend.herokuapp.com/login',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password
+      })
+    })
+    .then((resp) => resp.json())
+    .then((respJson)=>{
+      console.log(respJson, 'Just logged in')
+      if(respJson.success){
+        this.props.navigator.push({
+          component: Users,
+          title: "Users",
+          rightButtonTitle: 'Messages',
+          onRightButtonPress: this.messages
+        });
+      } else(
+        console.log('Error, username or password is incorrect')
+      )
+
+
+    })
+    .catch((err)=>{
+      console.error(err)
+    });
+
+  },
+  render(){
+    return (
+      <View style={styles.container} >
+        <Text style={styles.textBig}>Login</Text>
+        <TextInput
+          style={{height: 40}}
+          placeholder="Enter your username"
+          onChangeText={(text) => this.setState({username: text})}
+          value={this.state.username}
+        />
+        <TextInput
+          style={{height: 40}}
+          secureTextEntry={true}
+          placeholder="Enter your password"
+          onChangeText={(text) => this.setState({password: text})}
+          value={this.state.password}
+        />
+        <TouchableOpacity>
+          <Text style={{backgroundColor: 'red', color: 'white', width: 100 }} onPress={this.main}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+})
+
 var Login = React.createClass({
   press() {
-    
+    this.props.navigator.push({
+      component: LoginInput,
+      title: 'LoginPage'
+    })
+  },
+  main(){
+    fetch('https://hohoho-backend.herokuapp.com/register',{
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password
+      })
+    })
+    .then((resp) => resp.json())
+    .then((respJson)=>{
+      console.log(respJson, 'Just registered a user')
+      this.props.navigator.push({
+        component: Login,
+        title: "Login"
+      });
+
+    })
+    .catch((err)=>{
+      console.error(err)
+    });
+
   },
   register() {
     this.props.navigator.push({
@@ -89,7 +374,7 @@ const styles = StyleSheet.create({
     margin: 10,
   },
   button: {
-    alignSelf: 'stretch', 
+    alignSelf: 'stretch',
     paddingTop: 10,
     paddingBottom: 10,
     marginTop: 10,
@@ -98,7 +383,7 @@ const styles = StyleSheet.create({
     borderRadius: 5
   },
   buttonRed: {
-    backgroundColor: '#FF585B', 
+    backgroundColor: '#FF585B',
   },
   buttonBlue: {
     backgroundColor: '#0074D9',
