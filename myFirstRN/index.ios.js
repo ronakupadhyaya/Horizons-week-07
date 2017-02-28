3d49198dd40217628cf19935fbdd8f8d4de22f2e
Brian <snuupy@Brians-Mac.local>
2017-02-27 16:19:43 -0800

    textview

--- myFirstRN/index.ios.js
+++ myFirstRN/index.ios.js
@@ -22,15 +22,14 @@ export default class myFirstRN extends Component {
     this.state = {
       textSize: 40
     };
-    var dataSource = new ListView.DataSource({
-      rowHasChanged: (r1, r2) => (r1 !== r2)
-    });
+
     this.state = {
-      dataSource: dataSource.cloneWithRows(_.range(100))
+      //dataSource: dataSource.cloneWithRows(_.range(100))
+      data: _.range(100)
     };
 
   }
-  press(item){
+  press(item) {
     alert('Pressed ' + item);
   };
 
@@ -39,23 +38,51 @@ export default class myFirstRN extends Component {
       textSize: this.state.textSize + 5
     });
   }
-  render() {
-    return (
 
-      <View style={{
-        marginTop: 20,
-        flex: 1
-      }}>
-        <ListView renderRow={(item) => (
-          <View style={{
-            alignItems: 'center'
-          }}>
-            <TouchableOpacity onPress={this.press.bind(this, item)}>
-              <Text>{item}</Text>
-            </TouchableOpacity>
-          </View>
-        )} dataSource={this.state.dataSource}/>
+  remove(item) {
+    // console.log(this.state.dataSource.rowIdentities[0].splice(item, 1));
+    this.setState({
+      data: this.state.data.filter((curItem) => (item !== curItem))
+    });
+  }
+
+  render() {
+    var dataSource = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => (r1 !== r2)
+    });
+
+    return (
+      <View style={{flex: 1,
+          justifyContent: 'center', //vertically centered
+          alignItems: 'center' //horizontally centered
+        }}>
+        <Text>
+          I am centered
+        </Text>
       </View>
+
+    // 2/3rds
+      // <View style={{flex: 1}}>
+      //   <View style={{flex: 1, backgroundColor: 'red'}} ></View>
+      // <View style={{flex: 2, backgroundColor: 'blue'}} ></View>
+      // </View>
+
+    //removes item when clicked
+    // <View style={{
+    //   marginTop: 20,
+    //   flex: 1
+    // }}>
+    //   <ListView renderRow={(item) => (
+    //     <View style={{
+    //       alignItems: 'center'
+    //     }}>
+    //       <TouchableOpacity onPress={this.remove.bind(this, item)}>
+    //         <Text>{item}</Text>
+    //       </TouchableOpacity>
+    //     </View>
+    //   )} dataSource={dataSource.cloneWithRows(this.state.data)}/>
+    // </View>
+
     // <View style={{
     //   flex: 1,
     //   justifyContent: 'center',
