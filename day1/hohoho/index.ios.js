3cbf251d1585b6a38caec7667c7302dbb56302a1
lisaztruong <lisaztruong@gmail.com>
2017-03-01 17:17:50 -0800

    day 2

--- day1/hohoho/index.ios.js
+++ day1/hohoho/index.ios.js
@@ -8,7 +8,9 @@ import {
   TextInput,
   NavigatorIOS,
   Button,
-  ListView
+  ListView,
+  AsyncStorage,
+  MapView
 } from 'react-native'
 
 // This is the root view
@@ -123,27 +125,38 @@ var Register = React.createClass({
     render() {
       return (
         <View>
-          <Text>pls appear</Text>
-          <ListView
-          enableEmptySections={true}
-          dataSource={this.state.dataSource}
-          renderRow={(rowData) =>  (
-                <View>
-                  <Text>From {rowData.from.username}</Text>
-                  <Text>To {rowData.to.username}</Text>
-                  <Text>Message {rowData.body}</Text>
-                  <Text>When {rowData.timestamp}</Text>
-                </View>
-            )}
-          />
+        <ListView
+        enableEmptySections={true}
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) =>  (
+          <View>
+          <Text>From {rowData.from.username}</Text>
+          <Text>To {rowData.to.username}</Text>
+          <Text>Message {rowData.body}</Text>
+          {(rowData.hasOwnProperty("location"))?
+          <MapView
+           style={{width: 400, height: 150, margin: 40, alignItems: 'center'}}
+           scrollEnabled={true}
+           region={{
+             latitude: rowData.location.latitude,
+             longitude: rowData.location.longitude,
+             longitudeDelta: 1,
+             latitudeDelta: 1
+           }}
+           annotations={[{
+             latitude: rowData.location.latitude,
+             longitude: rowData.location.longitude,
+             title: "Pinged Location"
+           }]}
+         />
+            : null}
+          <Text>When {rowData.timestamp}</Text>
+          </View>
+        )}
+        />
         </View>
       );
     }
-
-
-
-
-
   })
 
 
@@ -192,16 +205,63 @@ var Register = React.createClass({
       })
       .catch((err)=>console.log("err",err))
     },
+
+    button(){
+      this.props.navigator.push({
+        component: Messages,
+        title: "Message",
+        passProps: {parent: this.props.parent}
+    })
+  },
+
+  sendLocation(rowData){
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        console.log("Got position:", position);
+        fetch('https://hohoho-backend.herokuapp.com/messages',{
+          method:'POST',
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            to: rowData._id,
+            location: {
+              longitude: position.coords.longitude, latitude: position.coords.latitude
+            }
+          })
+        })
+        .then((resp)=> {
+          console.log(resp)
+          return resp.json()
+        })
+        .then((responseJson)=>{
+          console.log(responseJson)
+          if(!responseJson.success){
+            alert("This is wrong")
+          } else {
+            alert("Your HO HO HO! has been sent to "+rowData.username)
+          }
+        })
+        .catch((err)=>console.log("err",err))
+      },
+      error => alert(error.message),
+      {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+    );
+  },
+
     render() {
       return(
         <View style={{flex:1, marginTop: 69}}>
-          <Messages />
+          <Button title= "Message" onPress={this.button} />
           <ListView
             enableEmptySections={true}
             dataSource={this.state.dataSource}
             renderRow={(rowData) =>  (
-              <TouchableOpacity onPress={this.touchUser.bind(this, rowData)}>
-                <Text style={{borderColor: "black", borderWidth: 2}}>{rowData.username}</Text>
+              <TouchableOpacity
+                onPress={this.touchUser.bind(this, rowData)}
+                onLongPress={this.sendLocation.bind(this, rowData)}
+                delayLongPress={500}>
+                  <Text style={{borderColor: "black", borderWidth: 2}}>{rowData.username}</Text>
               </TouchableOpacity>
             )}
           />
@@ -211,23 +271,45 @@ var Register = React.createClass({
   })
 
   var Login = React.createClass({
-    press() {
+    componentDidMount() {
+      AsyncStorage.getItem('user')
+        .then((result) => {
+        var parsedResult = JSON.parse(result);
+        var username = parsedResult.username;
+        var password = parsedResult.password;
+        if (username && password) {
+          return this.press(username,password)
+        }
+    });
+  },
+    press(username,password) {
+      var myusername = (typeof username !== 'string' ? this.props.parent.state.username : username)
+      var mypassword = (typeof password !== 'string' ? this.props.parent.state.password : password)
+      console.log("fuck",username,password)
       fetch('https://hohoho-backend.herokuapp.com/login', {
         method: 'POST',
         headers: {
           "Content-Type": "application/json"
         },
         body: JSON.stringify({
-          username: this.props.parent.state.username,
-          password: this.props.parent.state.password
+          username: myusername,
+          password: mypassword
         })
       })
       .then((response)=> response.json())
       .then((responseJson)=> {
         console.log(responseJson);
         if(!responseJson.success){
-          alert("This is wrong")
+          if(this.props.parent.state.username===""&&this.props.parent.state.password===""){
+            console.log("nothing entered yet")
+          } else {
+            alert("Wrong login credentials")
+          }
         }else {
+          AsyncStorage.setItem('user', JSON.stringify({
+            username: this.props.parent.state.username,
+            password: this.props.parent.state.password
+          }));
           this.props.navigator.push({
             component: Users,
             title: "Users",
@@ -248,26 +330,26 @@ var Register = React.createClass({
     render() {
       return (
         <View style={styles.container}>
-        <TextInput
-        style={{height: 40}}
-        placeholder="username"
-        defaultValue={this.props.parent.state.username}
-        onChangeText={(text) => this.props.parent.setState({username: text})}
-        />
-        <TextInput
-        style={{height: 40}}
-        placeholder="password"
-        secureTextEntry={true}
-        defaultValue={this.props.parent.state.password}
-        onChangeText={(text) => this.props.parent.setState({password: text})}
-        />
-        <Text style={styles.textBig}>Login to HoHoHo!</Text>
-        <TouchableOpacity onPress={this.press} style={[styles.button, styles.buttonGreen]}>
-        <Text style={styles.buttonLabel}>Tap to Login</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.button, styles.buttonBlue]} onPress={this.register}>
-        <Text style={styles.buttonLabel}>Tap to Register</Text>
-        </TouchableOpacity>
+          <TextInput
+            style={{height: 40}}
+            placeholder="username"
+            defaultValue={this.props.parent.state.username}
+            onChangeText={(text) => this.props.parent.setState({username: text})}
+            />
+          <TextInput
+            style={{height: 40}}
+            placeholder="password"
+            secureTextEntry={true}
+            defaultValue={this.props.parent.state.password}
+            onChangeText={(text) => this.props.parent.setState({password: text})}
+          />
+          <Text style={styles.textBig}>Login to HoHoHo!</Text>
+          <TouchableOpacity onPress={this.press} style={[styles.button, styles.buttonGreen]}>
+            <Text style={styles.buttonLabel}>Tap to Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.button, styles.buttonBlue]} onPress={this.register}>
+            <Text style={styles.buttonLabel}>Tap to Register</Text>
+          </TouchableOpacity>
         </View>
 
       );
