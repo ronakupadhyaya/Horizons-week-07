8fbae1226d063ff340d42cb9de7ecf531f6678ed
Corey Mason <corey.mason98@googlemail.com>
2017-07-09 13:48:43 -0700

    Fix press context

--- day1/hohoho/App.js
+++ day1/hohoho/App.js
@@ -27,10 +27,10 @@ class LoginScreen extends React.Component {
     return (
       <View style={styles.container}>
         <Text style={styles.textBig}>Login to HoHoHo!</Text>
-        <TouchableOpacity onPress={this.press} style={[styles.button, styles.buttonGreen]}>
+        <TouchableOpacity onPress={ () => {this.press()} } style={[styles.button, styles.buttonGreen]}>
           <Text style={styles.buttonLabel}>Tap to Login</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.button, styles.buttonBlue]} onPress={() => {this.register()} }>
+        <TouchableOpacity style={[styles.button, styles.buttonBlue]} onPress={ () => {this.register()} }>
           <Text style={styles.buttonLabel}>Tap to Register</Text>
         </TouchableOpacity>
       </View>
