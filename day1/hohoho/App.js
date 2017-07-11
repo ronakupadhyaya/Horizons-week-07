076a11683c6b1dcbabe8b05b5d0f44f13df8fb1f
vanessacortes <vcortes@berkeley.edu>
2017-07-11 09:26:29 -0700

    react-native app

--- day1/hohoho/App.js
+++ day1/hohoho/App.js
@@ -14,12 +14,45 @@ import { StackNavigator } from 'react-navigation';
 
 //Screens
 class LoginScreen extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: 'username',
+      password: 'password',
+      message: 'message'
+    }
+  }
   static navigationOptions = {
-    title: 'Login'
+    title: 'Home'
   };
 
   press() {
-
+    fetch('https://hohoho-backend.herokuapp.com/login', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password,
+      })
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      /* do something with responseJson and go back to the Login view but
+       * make sure to check for responseJson.success! */
+       if(responseJson.success){
+         this.props.navigation.navigate('Users')
+       }else{
+        //  this.setState({message: 'Could not login'})
+        //  <Text> {this.state.message} </Text>
+        alert('Could not login!')
+       }
+    })
+    .catch((err) => {
+      /* do something if there was an error with fetching */
+      console.log('error', err);
+    });
   }
   register() {
     this.props.navigation.navigate('Register');
@@ -28,12 +61,24 @@ class LoginScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.textBig}>Login to HoHoHo!</Text>
-        <TouchableOpacity onPress={ () => {this.press()} } style={[styles.button, styles.buttonGreen]}>
-          <Text style={styles.buttonLabel}>Tap to Login</Text>
+        <Text style={styles.textBig}>Login</Text>
+        <TextInput
+          style={styles.textBox}
+          placeholder="Enter your username"
+          onChangeText={(text) => this.setState({username: text})}
+        />
+        <TextInput
+          style={styles.textBox}
+          placeholder="Enter your password"
+          secureTextEntry={true}
+          onChangeText={(text) => this.setState({password: text})}
+        />
+        <TouchableOpacity style={[styles.button, styles.buttonGreen]} onPress={ () => {this.press()} }>
+          <Text style={styles.buttonLabel}>Login</Text>
         </TouchableOpacity>
+
         <TouchableOpacity style={[styles.button, styles.buttonBlue]} onPress={ () => {this.register()} }>
-          <Text style={styles.buttonLabel}>Tap to Register</Text>
+          <Text style={styles.buttonLabel}>Register</Text>
         </TouchableOpacity>
       </View>
     )
@@ -41,15 +86,117 @@ class LoginScreen extends React.Component {
 }
 
 class RegisterScreen extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: 'username',
+      password: 'password'
+    }
+  }
   static navigationOptions = {
     title: 'Register'
   };
+  press() {
+    fetch('https://hohoho-backend.herokuapp.com/register', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password,
+      })
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      /* do something with responseJson and go back to the Login view but
+       * make sure to check for responseJson.success! */
+       if(responseJson.success){
+         this.props.navigation.navigate('Login')
+       }
+    })
+    .catch((err) => {
+      /* do something if there was an error with fetching */
+      console.log('error', err);
+    });
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.textBig}>Register</Text>
+        <TextInput
+          style={styles.textBox}
+          placeholder="Enter your username"
+          onChangeText={(text) => this.setState({username: text})}
+        />
+        <TextInput
+          style={styles.textBox}
+          placeholder="Enter your password"
+          secureTextEntry={true}
+          onChangeText={(text) => this.setState({password: text})}
+        />
+        <TouchableOpacity style={[styles.button, styles.buttonBlue]} onPress={ () => {this.press()} }>
+          <Text style={styles.buttonLabel}>Register</Text>
+        </TouchableOpacity>
       </View>
+    )
+  }
+}
+
+class UsersScreen extends React.Component {
+  //navigationOptions code
+  static navigationOptions = {
+    title: 'Users' //you put the title you want to be displayed here
+  };
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows([
+        'Moose', 'Corey', 'Allie', 'Jay', 'Graham', 'Darwish', 'Abhi Fitness'
+      ])
+    };
+  }
+  touchUser(user) {
+    fetch('https://hohoho-backend.herokuapp.com/messages', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        to: user._id
+      })
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      /* do something with responseJson and go back to the Login view but
+       * make sure to check for responseJson.success! */
+       if(responseJson.success){
+         Alert.alert(
+          'Success',
+          'Sent!',
+          [{text: 'Your Ho Ho Ho! to '+ user.username + ' has been sent!'}] // Button
+        )
+       }else{
+         alert('Your Ho Ho Ho! to '+ user.username + ' could not be sent')
+       }
+    })
+    .catch((err) => {
+      /* do something if there was an error with fetching */
+      console.log('error', err);
+    });
+  }
+
+  render() {
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) =>
+          <TouchableOpacity onPress={this.touchUser.bind(this, rowData)}>
+            <Text>{rowData}</Text>
+          </TouchableOpacity>}
+      />
     )
   }
 }
@@ -63,6 +210,9 @@ export default StackNavigator({
   Register: {
     screen: RegisterScreen,
   },
+  Users: {
+    screen: UsersScreen
+  }
 }, {initialRouteName: 'Login'});
 
 
@@ -117,5 +267,15 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 16,
     color: 'white'
+  },
+  textBox: {
+    height: 40,
+    paddingTop: 10,
+    paddingBottom: 10,
+    marginTop: 10,
+    marginLeft: 5,
+    marginRight: 5,
+    borderRadius: 5,
+    borderStyle: 'solid'
   }
 });
