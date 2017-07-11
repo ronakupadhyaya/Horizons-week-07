d7d9db438388e70070f7db9ae63824969906d160
Sasuke1964 <neilperry1964@gmail.com>
2017-07-11 09:16:27 -0700

    partial progress from bing sick -- preparing for pull

--- day1/hohoho/App.js
+++ day1/hohoho/App.js
@@ -14,12 +14,38 @@ import { StackNavigator } from 'react-navigation';
 
 //Screens
 class LoginScreen extends React.Component {
+  constructor() {
+    super();
+    this.state = { username: "", password: "" };
+  }
+
   static navigationOptions = {
     title: 'Login'
   };
 
   press() {
-
+    fetch('https://hohoho-backend.herokuapp.com/login', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password
+      })
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        if (responseJson.success) {
+          this.props.navigation.navigate('Register');
+        } else {
+          alert("Database error!");
+        }
+      })
+      .catch((err) => {
+        /* do something if there was an error with fetching */
+        console.log(err);
+      });
   }
   register() {
     this.props.navigation.navigate('Register');
@@ -29,10 +55,21 @@ class LoginScreen extends React.Component {
     return (
       <View style={styles.container}>
         <Text style={styles.textBig}>Login to HoHoHo!</Text>
-        <TouchableOpacity onPress={ () => {this.press()} } style={[styles.button, styles.buttonGreen]}>
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="username"
+          onChangeText={(text) => this.setState({ username: text })}
+        />
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="password"
+          secureTextEntry={true}
+          onChangeText={(text) => this.setState({ password: text })}
+        />
+        <TouchableOpacity onPress={() => { this.press() }} style={[styles.button, styles.buttonGreen]}>
           <Text style={styles.buttonLabel}>Tap to Login</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={[styles.button, styles.buttonBlue]} onPress={ () => {this.register()} }>
+        <TouchableOpacity style={[styles.button, styles.buttonBlue]} onPress={() => { this.register() }}>
           <Text style={styles.buttonLabel}>Tap to Register</Text>
         </TouchableOpacity>
       </View>
@@ -41,14 +78,58 @@ class LoginScreen extends React.Component {
 }
 
 class RegisterScreen extends React.Component {
+  constructor() {
+    super();
+    this.state = { username: "", password: "" };
+  }
+
   static navigationOptions = {
     title: 'Register'
   };
+  onSubmit() {
+    fetch('https://hohoho-backend.herokuapp.com/register', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password,
+      })
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        /* do something with responseJson and go back to the Login view but
+         * make sure to check for responseJson.success! */
+        if (responseJson.success) {
+          this.props.navigation.goBack();
+        } else {
+          alert('Database Error')
+        }
+      })
+      .catch((err) => {
+        /* do something if there was an error with fetching */
+        console.log(err);
+      });
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.textBig}>Register</Text>
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="Enter your username"
+          onChangeText={(text) => this.setState({ username: text })}
+        />
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="Enter your password"
+          onChangeText={(text) => this.setState({ password: text })}
+        />
+        <TouchableOpacity onPress={() => this.onSubmit()}>
+          <Text style={[styles.button, styles.buttonBlue]}>Submit</Text>
+        </TouchableOpacity>
       </View>
     )
   }
@@ -63,7 +144,7 @@ export default StackNavigator({
   Register: {
     screen: RegisterScreen,
   },
-}, {initialRouteName: 'Login'});
+}, { initialRouteName: 'Login' });
 
 
 //Styles
