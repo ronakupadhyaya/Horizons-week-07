b7b9b06d284a359211eee6e23b0bc3ce75bc2765
allaunyc <alau11@binghamton.edu>
2017-07-11 09:31:49 -0700

    part 1 hohoho

--- day1/hohoho/App.js
+++ day1/hohoho/App.js
@@ -18,8 +18,36 @@ class LoginScreen extends React.Component {
     title: 'Login'
   };
 
-  press() {
+  constructor(props) {
+    super(props)
+    this.state = {
+      error: ""
+    }
+  }
 
+  press() {
+    fetch('https://hohoho-backend.herokuapp.com/login', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password,
+      })
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      if(responseJson.success) {
+        this.props.navigation.navigate('UsersList');
+      } else {
+        this.setState({error: 'Failed to login'})
+      }
+
+    })
+    .catch((err) => {
+      console.log('error: ', err);
+    });
   }
   register() {
     this.props.navigation.navigate('Register');
@@ -29,6 +57,21 @@ class LoginScreen extends React.Component {
     return (
       <View style={styles.container}>
         <Text style={styles.textBig}>Login to HoHoHo!</Text>
+
+        <Text>{this.state.error}</Text>
+
+        <TextInput
+          style={{width: 200, height: 40}}
+          placeholder="Enter your username"
+          onChangeText={(text) => this.setState({username: text})}
+        />
+
+        <TextInput
+          style={{width: 200, height: 40}}
+          placeholder="Enter your password"
+          onChangeText={(text) => this.setState({password: text})}
+        />
+
         <TouchableOpacity onPress={ () => {this.press()} } style={[styles.button, styles.buttonGreen]}>
           <Text style={styles.buttonLabel}>Tap to Login</Text>
         </TouchableOpacity>
@@ -45,11 +88,90 @@ class RegisterScreen extends React.Component {
     title: 'Register'
   };
 
+  press() {
+    fetch('https://hohoho-backend.herokuapp.com/register', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password,
+      })
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log('result', responseJson);
+      this.props.navigation.goBack();
+    })
+    .catch((err) => {
+      console.log('error: ', err);
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.textBig}>Register</Text>
+
+        <TextInput
+          style={{width: 200, height: 40}}
+          placeholder="Enter your username"
+          onChangeText={(text) => this.setState({username: text})}
+        />
+
+        <TextInput
+          style={{width: 200, height: 40}}
+          placeholder="Enter your password"
+          onChangeText={(text) => this.setState({password: text})}
+        />
+
+        <TouchableOpacity style={[styles.button, styles.buttonBlue]} onPress={() => this.press()}>
+          <Text>Tap to register!</Text>
+        </TouchableOpacity>
       </View>
+    )
+  }
+}
+
+class UsersScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Users List'
+  };
+
+  constructor(props) {
+    super(props);
+
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows([])
+    };
+
+    fetch('https://hohoho-backend.herokuapp.com/users', {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log('result', responseJson);
+      this.setState({
+        dataSource: ds.cloneWithRows(responseJson.users)
+      })
+    })
+    .catch((err) => {
+      console.log('error: ', err);
+    });
+
+  }
+
+  render() {
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={(rowData) => <Text>{rowData.username}</Text>}
+      />
     )
   }
 }
@@ -62,6 +184,9 @@ export default StackNavigator({
   },
   Register: {
     screen: RegisterScreen,
+  },
+  UsersList: {
+    screen: UsersScreen,
   },
 }, {initialRouteName: 'Login'});
 
