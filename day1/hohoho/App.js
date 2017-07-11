994ec69b28d3f64a644fb9fc73eac3b439044b73
kritpetra <krit.petra@gmail.com>
2017-07-11 09:16:03 -0700

    Finished register page

--- day1/hohoho/App.js
+++ day1/hohoho/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import {
-  StyleSheet,
   View,
   Text,
   TouchableOpacity,
   TextInput,
   ListView,
   Alert,
-  Button
+  Button,
+  ActivityIndicator,
+  Modal
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+
+import styles from './styles';
 
 
 //Screens
@@ -18,25 +21,77 @@ class LoginScreen extends React.Component {
     title: 'Login'
   };
 
-  press() {
+  constructor() {
+    super();
+    this.state = {
+      loginFormVisible: false,
+      username: '',
+      password: '',
+      loading: false
+    };
+  }
 
+  toggleLoginModal(bool) {
+    this.setState({ loginFormVisible: bool });
   }
   register() {
     this.props.navigation.navigate('Register');
+  }
+  loginUser() {
+    alert("Logging in as "+this.state.username);
   }
 
   render() {
     return (
       <View style={styles.container}>
+
+        <Modal
+          animationType="slide"
+          transparent="false"
+          visible={this.state.loginFormVisible}
+          onRequestClose={() => this.toggleLoginModal(false)}>
+
+          <View style={styles.container}>
+            <TextInput
+              style={styles.formInput}
+              placeholder="Username"
+              onChangeText={text => this.setState({ username: text })}
+              value={this.state.username}
+            />
+            <TextInput
+              style={styles.formInput}
+              placeholder="Password"
+              onChangeText={text => this.setState({ password: text })}
+              value={this.state.password}
+            />
+            <TouchableOpacity>
+              <Button
+                onPress={this.loginUser.bind(this)}
+                title="Login"
+                color="#2ECC40"
+                accessibilityLabel="Log into HoHoHo with the entered credentials"
+              />
+            </TouchableOpacity>
+
+          </View>
+          </Modal>
+
         <Text style={styles.textBig}>Login to HoHoHo!</Text>
-        <TouchableOpacity onPress={ () => {this.press()} } style={[styles.button, styles.buttonGreen]}>
+        <TouchableOpacity onPress={ () => this.toggleLoginModal(true) } style={[styles.button, styles.buttonGreen]}>
           <Text style={styles.buttonLabel}>Tap to Login</Text>
         </TouchableOpacity>
         <TouchableOpacity style={[styles.button, styles.buttonBlue]} onPress={ () => {this.register()} }>
           <Text style={styles.buttonLabel}>Tap to Register</Text>
         </TouchableOpacity>
+
+        {this.state.loading &&
+          <View style={styles.loadingScreen}>
+            <View style={styles.darkOverlay}><Text></Text></View>
+            <ActivityIndicator size='large' />
+          </View>}
+
       </View>
-    )
+    );
   }
 }
 
@@ -45,12 +100,69 @@ class RegisterScreen extends React.Component {
     title: 'Register'
   };
 
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: "",
+      loading: false
+    };
+  }
+
+  registerUser() {
+    this.setState({ loading: true })
+    fetch('https://hohoho-backend.herokuapp.com/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password
+      })
+    })
+    .then(resp => resp.json())
+    .then(() => this.props.navigation.goBack())
+    .catch(err => alert("An error occurred while trying to register: " + err))
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.textBig}>Register</Text>
+
+        <TextInput
+          style={styles.formInput}
+          placeholder="Username"
+          onChangeText={text => this.setState({ username: text })}
+          value={this.state.username}
+        />
+
+        <TextInput
+          style={styles.formInput}
+          placeholder="Password"
+          onChangeText={text => this.setState({ password: text })}
+          value={this.state.password}
+        />
+
+        <TouchableOpacity>
+          <Button
+            onPress={this.registerUser.bind(this)}
+            title="Register"
+            color="#0074D9"
+            accessibilityLabel="Register with the entered credentials"
+          />
+        </TouchableOpacity>
+
+        {this.state.loading &&
+          <View style={styles.loadingScreen}>
+            <View style={styles.darkOverlay}><Text></Text></View>
+            <ActivityIndicator size='large' />
+          </View>}
+
       </View>
-    )
+
+
+    );
   }
 }
 
@@ -63,59 +175,4 @@ export default StackNavigator({
   Register: {
     screen: RegisterScreen,
   },
-}, {initialRouteName: 'Login'});
-
-
-//Styles
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  containerFull: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'stretch',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  textBig: {
-    fontSize: 36,
-    textAlign: 'center',
-    margin: 10,
-  },
-  button: {
-    alignSelf: 'stretch',
-    paddingTop: 10,
-    paddingBottom: 10,
-    marginTop: 10,
-    marginLeft: 5,
-    marginRight: 5,
-    borderRadius: 5
-  },
-  buttonRed: {
-    backgroundColor: '#FF585B',
-  },
-  buttonBlue: {
-    backgroundColor: '#0074D9',
-  },
-  buttonGreen: {
-    backgroundColor: '#2ECC40'
-  },
-  buttonLabel: {
-    textAlign: 'center',
-    fontSize: 16,
-    color: 'white'
-  }
-});
+}, { initialRouteName: 'Login' });
---
+++ day1/hohoho/styles.js
@@ -0,0 +1,86 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  containerFull: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'stretch',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  textBig: {
+    fontSize: 36,
+    textAlign: 'center',
+    margin: 10,
+  },
+  button: {
+    alignSelf: 'stretch',
+    paddingTop: 10,
+    paddingBottom: 10,
+    marginTop: 10,
+    marginLeft: 5,
+    marginRight: 5,
+    borderRadius: 5
+  },
+  buttonRed: {
+    backgroundColor: '#FF585B'
+  },
+  buttonBlue: {
+    backgroundColor: '#0074D9'
+  },
+  buttonGreen: {
+    backgroundColor: '#2ECC40'
+  },
+  buttonLabel: {
+    textAlign: 'center',
+    fontSize: 16,
+    color: 'white'
+  },
+
+  formInput: {
+    height: 50,
+    width: '50%',
+    borderRadius: 4,
+    borderWidth: 0.5,
+    borderColor: 'lightblue'
+  },
+
+  loadingScreen: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+
+  darkOverlay: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'black',
+    opacity: 0.25,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+});
