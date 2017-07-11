fe626dc1f11120a2a84f2daa9b346ac30daa28e4
Nicholas Park <nickpark1997@gmail.com>
2017-07-11 09:19:56 -0700

    commit

--- day1/hohoho/App.js
+++ day1/hohoho/App.js
@@ -14,13 +14,42 @@ import { StackNavigator } from 'react-navigation';
 
 //Screens
 class LoginScreen extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: 'string',
+      password: 'string'
+    }
+  }
   static navigationOptions = {
     title: 'Login'
   };
 
   press() {
-
+    fetch('https://hohoho-backend.herokuapp.com/login', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password,
+      })
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      console.log(responseJson, responseJson.success)
+      if (responseJson.success) {
+        this.props.navigation.navigate('Users');
+      } else {
+        alert("Couldn't log in");
+      }
+    })
+    .catch((err) => {
+      console.log('Error:', err);
+    })
   }
+
   register() {
     this.props.navigation.navigate('Register');
   }
@@ -29,6 +58,17 @@ class LoginScreen extends React.Component {
     return (
       <View style={styles.container}>
         <Text style={styles.textBig}>Login to HoHoHo!</Text>
+        <TextInput
+          style={styles.textBox}
+          placeholder="Enter your username"
+          onChangeText={(text) => this.setState({username: text})}
+        />
+        <TextInput
+          style={styles.textBox}
+          placeholder="Enter your password"
+          onChangeText={(text) => this.setState({password: text})}
+          secureTextEntry={true}
+        />
         <TouchableOpacity onPress={ () => {this.press()} } style={[styles.button, styles.buttonGreen]}>
           <Text style={styles.buttonLabel}>Tap to Login</Text>
         </TouchableOpacity>
@@ -41,19 +81,137 @@ class LoginScreen extends React.Component {
 }
 
 class RegisterScreen extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      username: 'string',
+      password: 'string'
+    };
+  }
   static navigationOptions = {
     title: 'Register'
   };
+
+  press() {
+    fetch('https://hohoho-backend.herokuapp.com/register', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password,
+      })
+    })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      if (responseJson.success) {
+        this.props.navigation.navigate('Login');
+      } else {
+        alert("Couldn't register");
+      }
+    })
+    .catch((err) => {
+      console.log('Error:', err);
+    })
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.textBig}>Register</Text>
+        <TextInput
+          style={styles.textBox}
+          placeholder="Enter your username"
+          onChangeText={(text) => this.setState({username: text})}
+        />
+        <TextInput
+          style={styles.textBox}
+          placeholder="Enter your password"
+          onChangeText={(text) => this.setState({password: text})}
+          secureTextEntry={true}
+        />
+        <TouchableOpacity onPress={ () => {this.press()} } style={[styles.button, styles.buttonBlue]}>
+          <Text style={styles.buttonLabel}>Register</Text>
+        </TouchableOpacity>
       </View>
     )
   }
 }
 
+class UsersScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Users' //you put the title you want to be displayed here
+  };
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows([
+        'Moose', 'Corey', 'Allie', 'Jay', 'Graham', 'Darwish', 'Abhi Fitness'
+      ])
+    };
+  }
+  touchUser(user) {
+    console.log(user);
+    fetch('https://hohoho-backend.herokuapp.com/users', {
+      method: 'GET',
+    })
+    .then(response => response.json()) //make it readable to machine
+    .then(responseJson => {
+      if (responseJson.success) {
+        var theUser;
+        responseJson.users.forEach((aUser) => {
+          if (aUser.username === user) {
+            theUser = aUser;
+          }
+        });
+        fetch('https://hohoho-backend.herokuapp.com/messages', {
+          method: 'POST',
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            to: theUser._id,
+          })
+        })
+      }
+    })
+    .then((response) => {console.log('response', response.json()); console.log('id', user._id); return response.json()})
+    .then((responseJson) => {
+      if (responseJson.success) {
+        Alert.alert(
+          'Ho Ho Ho sent',
+          'Congrats',
+          [{text: 'Okay'}] // Button
+        )
+      } else {
+        Alert.alert(
+          "Couldn't send",
+          'Congrats',
+          [{text: 'Okay'}] // Button
+        )
+      }
+    })
+    .catch((err) => {
+      console.log('Error:', err);
+    })
+  }
+  render() {
+    return (
+      <View>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) =>
+            <TouchableOpacity onPress={this.touchUser.bind(this, rowData)}>
+              <Text>{rowData}</Text>
+            </TouchableOpacity>
+          }
+        />
+      </View>
+    )
+  }
+}
 
 //Navigator
 export default StackNavigator({
@@ -62,6 +220,9 @@ export default StackNavigator({
   },
   Register: {
     screen: RegisterScreen,
+  },
+  Users: {
+    screen: UsersScreen
   },
 }, {initialRouteName: 'Login'});
 
@@ -117,5 +278,16 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 16,
     color: 'white'
+  },
+  textBox: {
+    height: 40,
+    paddingTop: 10,
+    paddingBottom: 10,
+    marginTop: 10,
+    marginLeft: 5,
+    marginRight: 5,
+    borderRadius: 5,
+    borderStyle: 'solid',
+    borderColor: 'grey'
   }
 });
