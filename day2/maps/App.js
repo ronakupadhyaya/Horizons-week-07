3c5e8b4c919f46464fcbb460493c1628d3a1bdb8
JingyW <wangj2@whitman.edu>
2017-07-11 10:11:14 -0700

    morning stuff

--- day2/maps/App.js
+++ day2/maps/App.js
@@ -3,7 +3,8 @@ import {
   TouchableOpacity,
   StyleSheet,
   Text,
-  View
+  View,
+  AsyncStorage,
  } from 'react-native';
 
 import {
@@ -11,6 +12,34 @@ import {
 } from 'expo';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      longitude: 0,
+      latitude: 0,
+    }
+  };
+
+  componentDidMount() {
+    AsyncStorage.getItem('longitude')
+      .then(result=> {
+        this.setState({longitude:parseFloat(result)})
+      });
+    AsyncStorage.getItem('latitude')
+      .then(result => {
+        this.setState({latitude:parseFloat(result)})
+      })
+  }
+
+  changeLocation(region) {
+    console.log("changed");
+    console.log(region);
+    AsyncStorage.setItem('latitude',String(region.latitude))
+      .then(() => this.setState(latitude: region.latitude));
+    AsyncStorage.setItem('longitude',String(region.longitude))
+      .then(() => this.setState(longitude:region.longitude));
+  }
+
   render() {
     return (
       <View style={{
@@ -21,25 +50,56 @@ class App extends React.Component {
             style={{flex: 1,
               borderWidth: 1,
               alignItems: 'center',
-              justifyContent: 'center'}}>
+              justifyContent: 'center'}}
+            onPress={()=>{this.setState({latitude:41.067841,longitude:29.045258})}}>
             <Text>Istanbul</Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={{flex: 1,
               borderWidth: 1,
               alignItems: 'center',
-              justifyContent: 'center'}}>
+              justifyContent: 'center'}}
+            onPress={()=>{this.setState({latitude:-33.866174,longitude:151.220345})}}>
             <Text>Sydney</Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={{flex: 1,
               borderWidth: 1,
               alignItems: 'center',
-              justifyContent: 'center'}}>
+              justifyContent: 'center'}}
+              onPress={()=>{this.setState({latitude:22.294074,longitude:114.171995})}}>
             <Text>Hong Kong</Text>
           </TouchableOpacity>
+          <TouchableOpacity
+            style={{flex: 1,
+              borderWidth: 1,
+              alignItems: 'center',
+              justifyContent: 'center'}}
+              onPress={()=>{
+                navigator.geolocation.getCurrentPosition(
+                  (success) => {
+                    this.setState({
+                      latitude: parseFloat(success.coords.latitude),
+                      longitude: parseFloat(success.coords.longitude),
+                    });
+                  },
+                  (error) => {
+                  },
+                  {}
+                )
+              }}>
+            <Text>Here</Text>
+          </TouchableOpacity>
         </View>
-        <MapView style={{flex: 7}} />
+        <MapView
+          region={{
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+            latitudeDelta: 0.25,
+            longitudeDelta: 0.25,
+          }}
+          onRegionChange={this.changeLocation.bind(this)}
+          style={{flex: 7}} />
       </View>
     );
   }
