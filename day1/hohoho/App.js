a7d6c713c0ae4ce8f7e2819ac76d4d5f448d0ee7
Nicholas Park <nickpark1997@gmail.com>
2017-07-12 11:02:25 -0700

    commit

--- day1/hohoho/App.js
+++ day1/hohoho/App.js
@@ -7,9 +7,11 @@ import {
   TextInput,
   ListView,
   Alert,
-  Button
+  Button,
+  AsyncStorage
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import { Location, Permissions, MapView } from 'expo';
 
 
 //Screens
@@ -38,8 +40,14 @@ class LoginScreen extends React.Component {
     })
     .then((response) => response.json())
     .then((responseJson) => {
-      console.log(responseJson, responseJson.success)
       if (responseJson.success) {
+        AsyncStorage.setItem(
+          'user',
+          JSON.stringify({
+            username: this.state.username,
+            password: this.state.password
+          })
+        );
         this.props.navigation.navigate('Users');
       } else {
         alert("Couldn't log in");
@@ -52,6 +60,42 @@ class LoginScreen extends React.Component {
 
   register() {
     this.props.navigation.navigate('Register');
+  }
+
+  login(username, password) {
+    return fetch('https://hohoho-backend.herokuapp.com/login', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username: username,
+        password: password,
+      })
+    })
+  }
+
+  check(attempt) {
+    if (attempt.success) {
+      this.props.navigation.navigate('Users');
+    }
+  }
+
+  componentDidMount() {
+    AsyncStorage.getItem('user')
+    .then(result => {
+      var parsedResult = JSON.parse(result);
+      console.log('RESULT', parsedResult)
+      var username = parsedResult.username;
+      var password = parsedResult.password;
+      if (username && password) {
+        this.login(username, password)
+          .then(resp => resp.json())
+          .then(respJson => {this.check(respJson)});
+      }
+      // Don't really need an else clause, we don't do anything in this case.
+    })
+    .catch(err => {console.log(err)})
   }
 
   render() {
@@ -140,55 +184,75 @@ class RegisterScreen extends React.Component {
 }
 
 class UsersScreen extends React.Component {
-  static navigationOptions = {
-    title: 'Users' //you put the title you want to be displayed here
-  };
+  static navigationOptions = ({ navigation }) => ({
+    title: 'Users',
+    headerRight: <Button title='Messages' onPress={ () => {navigation.state.params.onRightPress()} } />
+  });
   constructor(props) {
     super(props);
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
-      dataSource: ds.cloneWithRows([
-        'Moose', 'Corey', 'Allie', 'Jay', 'Graham', 'Darwish', 'Abhi Fitness'
-      ])
+      dataSource: ds.cloneWithRows([])
     };
   }
-  touchUser(user) {
-    console.log(user);
-    fetch('https://hohoho-backend.herokuapp.com/users', {
-      method: 'GET',
-    })
-    .then(response => response.json()) //make it readable to machine
+
+
+  message() {
+    this.props.navigation.navigate('Messages');
+  }
+
+  sendLocation = async(user) => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      Alert.alert(
+        'Location services required!',
+        'Please',
+        [{text: 'Okay'}]
+      )
+    }
+    let location = await Location.getCurrentPositionAsync({enableHighAccuracy: true});
+    this.touchUser(user, location);
+  }
+
+  touchUser(user, location) {
+    let goFetch;
+    if (location.coords) {
+      goFetch = fetch('https://hohoho-backend.herokuapp.com/messages', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          to: user._id,
+          location: {
+            longitude: location.coords.longitude,
+            latitude: location.coords.latitude
+          }
+        })
+      })
+    } else {
+      goFetch = fetch('https://hohoho-backend.herokuapp.com/messages', {
+        method: 'POST',
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          to: user._id,
+        })
+      })
+    }
+    goFetch.then(response => response.json())
     .then(responseJson => {
       if (responseJson.success) {
-        var theUser;
-        responseJson.users.forEach((aUser) => {
-          if (aUser.username === user) {
-            theUser = aUser;
-          }
-        });
-        fetch('https://hohoho-backend.herokuapp.com/messages', {
-          method: 'POST',
-          headers: {
-            "Content-Type": "application/json"
-          },
-          body: JSON.stringify({
-            to: theUser._id,
-          })
-        })
-      }
-    })
-    .then((response) => {console.log('response', response.json()); console.log('id', user._id); return response.json()})
-    .then((responseJson) => {
-      if (responseJson.success) {
         Alert.alert(
-          'Ho Ho Ho sent',
+          'SENT',
           'Congrats',
           [{text: 'Okay'}] // Button
         )
       } else {
         Alert.alert(
           "Couldn't send",
-          'Congrats',
+          'Sorry',
           [{text: 'Okay'}] // Button
         )
       }
@@ -197,20 +261,117 @@ class UsersScreen extends React.Component {
       console.log('Error:', err);
     })
   }
+
+  componentDidMount() {
+    fetch('https://hohoho-backend.herokuapp.com/users', {
+      method: 'GET',
+    })
+    .then(response => response.json()) //make it readable to machine
+    .then(responseJson => {
+      const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+      this.setState({
+        dataSource: ds.cloneWithRows(responseJson.users)
+      })
+    })
+    this.props.navigation.setParams({
+      onRightPress: this.message.bind(this)
+    })
+  }
   render() {
     return (
-      <View>
+      <View style={styles.flexContainer}>
+        <View style={styles.user1}>
+          <Text>Nick</Text>
+        </View>
+        <View style={styles.user2}>
+          <Text>Jillian</Text>
+        </View>
+        <View style={styles.user3}>
+          <Text>Lizzie</Text>
+        </View>
         <ListView
+          style={styles.flexContainer}
           dataSource={this.state.dataSource}
           renderRow={(rowData) =>
-            <TouchableOpacity onPress={this.touchUser.bind(this, rowData)}>
-              <Text>{rowData}</Text>
+            <TouchableOpacity
+              onPress={this.touchUser.bind(this, rowData)}
+              onLongPress={this.sendLocation.bind(this, rowData)}
+              delayLongPress={1000}
+            >
+              <Text
+                style={styles.users}
+                >{rowData.username}</Text>
             </TouchableOpacity>
           }
         />
       </View>
     )
   }
+}
+
+class MessagesScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Messages' //you put the title you want to be displayed here
+  };
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows([])
+    };
+  }
+
+  showMap(location) {
+    if (location && location.longitude) {
+      return (
+        <MapView
+          style={{
+            width: 400,
+            height: 100
+          }}
+          showsUserLocation={true}
+          scrollEnabled={false}
+          region={{
+            longitude: location.longitude,
+            latitude: location.latitude,
+            longitudeDelta: .05,
+            latitudeDelta: .025
+          }}
+        />
+      )
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://hohoho-backend.herokuapp.com/messages', {
+      method: 'GET',
+    })
+    .then(response => response.json()) //make it readable to machine
+    .then(responseJson => {
+      const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+      this.setState({
+        dataSource: ds.cloneWithRows(responseJson.messages)
+      });
+    })
+  }
+  render() {
+    return (
+      <View>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) =>
+            <View>
+              <Text>
+                From {rowData.from.username} to {rowData.to.username} at {rowData.timestamp}: {rowData.body}
+              </Text>
+              {this.showMap(rowData.location)}
+            </View>
+          }
+        />
+      </View>
+    )
+  }
+
 }
 
 //Navigator
@@ -223,6 +384,9 @@ export default StackNavigator({
   },
   Users: {
     screen: UsersScreen
+  },
+  Messages: {
+    screen: MessagesScreen
   },
 }, {initialRouteName: 'Login'});
 
@@ -289,5 +453,24 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     borderStyle: 'solid',
     borderColor: 'grey'
+  },
+  flexContainer: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  user1: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'powderblue'
+  },
+  user2: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'skyblue'
+  },
+  user3: {
+    width: 200,
+    height: 200,
+    backgroundColor: 'steelblue'
   }
 });
--- day2/maps/App.js
+++ day2/maps/App.js
@@ -3,7 +3,8 @@ import {
   TouchableOpacity,
   StyleSheet,
   Text,
-  View
+  View,
+  AsyncStorage
  } from 'react-native';
 
 import {
@@ -11,6 +12,74 @@ import {
 } from 'expo';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      latitude: 41.067841,
+      longitude: 29.045258,
+      latitudeDelta: .05,
+      longitudeDelta: .025
+    }
+  }
+
+  istanbul() {
+    this.setState({
+      latitude: 41.067841,
+      longitude: 29.045258,
+      latitudeDelta: .05,
+      longitudeDelta: .025
+    })
+  }
+
+  sydney() {
+    this.setState({
+      latitude: -33.866174,
+      longitude: 151.220345,
+      latitudeDelta: .05,
+      longitudeDelta: .025
+    })
+  }
+
+  hongKong() {
+    this.setState({
+      latitude: 22.294074,
+      longitude: 114.171995,
+      latitudeDelta: .05,
+      longitudeDelta: .025
+    })
+  }
+
+  here() {
+    navigator.geolocation.getCurrentPosition(
+      (success) => {
+        this.setState({
+          latitude: success.coords.latitude,
+          longitude: success.coords.longitude,
+          latitudeDelta: .05,
+          longitudeDelta: .025
+        })
+      },
+      (err) => {},
+      {}
+    )
+  }
+
+  save(region) {
+  // this.setState({
+  //   latitudeDelta: region.latitudeDelta,
+  //   longitudeDelta: region.longitudeDelta
+  // })
+    console.log(region)
+    AsyncStorage.setItem('region', JSON.stringify(region))
+  }
+  componentDidMount() {
+    AsyncStorage.getItem('region')
+      .then(region => {
+        console.log('REGION', region)
+        this.setState(JSON.parse(region))
+      })
+  }
+
   render() {
     return (
       <View style={{
@@ -21,25 +90,49 @@ class App extends React.Component {
             style={{flex: 1,
               borderWidth: 1,
               alignItems: 'center',
-              justifyContent: 'center'}}>
+              justifyContent: 'center'}}
+            onPress={this.istanbul.bind(this)}
+          >
             <Text>Istanbul</Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={{flex: 1,
               borderWidth: 1,
               alignItems: 'center',
-              justifyContent: 'center'}}>
+              justifyContent: 'center'}}
+            onPress={this.sydney.bind(this)}
+          >
             <Text>Sydney</Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={{flex: 1,
               borderWidth: 1,
               alignItems: 'center',
-              justifyContent: 'center'}}>
+              justifyContent: 'center'}}
+            onPress={this.hongKong.bind(this)}
+          >
             <Text>Hong Kong</Text>
           </TouchableOpacity>
+          <TouchableOpacity
+            style={{flex: 1,
+              borderWidth: 1,
+              alignItems: 'center',
+              justifyContent: 'center'}}
+            onPress={this.here.bind(this)}
+          >
+            <Text>Here</Text>
+          </TouchableOpacity>
         </View>
-        <MapView style={{flex: 7}} />
+        <MapView
+          style={{flex: 7}}
+          region={{
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+            latitudeDelta: this.state.latitudeDelta,
+            longitudeDelta: this.state.longitudeDelta
+          }}
+          onRegionChange={this.save.bind(this)}
+        />
       </View>
     );
   }
