26c7045fb53742fd718562eec7d11e16e48b2efd
oadeboye <oadeboye@wellesley.edu>
2017-07-12 10:35:02 -0700

    Hohoho and Map exercises with jack and david

--- day1/hohoho/App.js
+++ day1/hohoho/App.js
@@ -7,10 +7,16 @@ import {
   TextInput,
   ListView,
   Alert,
-  Button
+  Button,
+  Image,
+  AsyncStorage
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-
+import {
+  Location,
+  Permissions,
+  MapView
+} from 'expo';
 
 //Screens
 class LoginScreen extends React.Component {
@@ -22,11 +28,57 @@ class LoginScreen extends React.Component {
       password: '',
       message: false,
     }
+    this.login = this.login.bind(this);
+
   }
 
   static navigationOptions = {
     title: 'Login'
   };
+
+  componentDidMount(){
+    AsyncStorage.getItem('user')
+    .then(result => {
+      var parsedResult = JSON.parse(result);
+      var username = parsedResult.username;
+      var password = parsedResult.password;
+      if (username && password) {
+        return this.login(username, password);
+      }
+      // Don't really need an else clause, we don't do anything in this case.
+    })
+    .catch(err => {
+      console.log('error mounting component: ', err);
+    });
+  }
+
+  login(username, password){
+    fetch('https://hohoho-backend.herokuapp.com/login', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username,
+        password,
+      })
+    })
+    .then(resp => resp.json())
+    .then((responseJ) => {
+      if (responseJ.success){
+        this.setState({
+          username,
+          password,
+        });
+        this.props.navigation.navigate('Users');
+      } else {
+        console.log('error', responseJ.error);
+      }
+    })
+    .catch((err) => {
+      console.log('something went wrong', err);
+    });
+  }
 
   press() {
     fetch('https://hohoho-backend.herokuapp.com/login', {
@@ -42,8 +94,14 @@ class LoginScreen extends React.Component {
     .then((response) => response.json())
     .then((responseJson) => {
       if (responseJson.success){
-        this.props.navigation.navigate('Users');
-        console.log('success!!!');
+        AsyncStorage.setItem('user', JSON.stringify({
+          username: this.state.username,
+          password: this.state.password
+        }))
+        .then(() => {
+          this.props.navigation.navigate('Users');
+          console.log('success!!!');
+        })
       } else {
         this.setState({message: 'invalid login, sorry'})
       }
@@ -58,7 +116,11 @@ class LoginScreen extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.textBig}>Smack Dat!</Text>
+        <Image
+          source={require('./img/smack.png')}
+          style={styles.logo}
+        />
+        <Text style={styles.textBig}>Smack Dat</Text>
         <TextInput
           style={styles.register}
           placeholder="Enter a username"
@@ -71,12 +133,12 @@ class LoginScreen extends React.Component {
           secureTextEntry={true}
           onChangeText={(text) => this.setState({password: text})}
         />
-        <TouchableOpacity onPress={ () => {this.press()} } style={[styles.button, styles.buttonGreen]}>
+        <TouchableOpacity onPress={ () => {this.press()} } style={[styles.button, styles.buttonPurple]}>
           <Text style={styles.buttonLabel}>Tap to Login</Text>
         </TouchableOpacity>
 
         <TouchableOpacity style={[styles.button, styles.buttonWhite]} onPress={ () => {this.register()} }>
-          <Text style={styles.buttonLabelGreen}>Tap to Register</Text>
+          <Text style={styles.buttonLabelPurple}>Tap to Register</Text>
         </TouchableOpacity>
       </View>
     )
@@ -124,7 +186,7 @@ class RegisterScreen extends React.Component {
           .then((response) => response.json())
           .then((responseJson) => {
             if(responseJson.success){
-              console.log(responseJson.success);
+              // console.log(responseJson.success);
               this.props.navigation.goBack();
             } else {
               console.log('something went wrong');
@@ -134,11 +196,11 @@ class RegisterScreen extends React.Component {
             console.log('error', err);
           });
         }} >
-          <Text style={styles.textBig}>Register</Text>
-        </TouchableOpacity>
-      </View>
-    )
-  }
+        <Text style={styles.textBig}>Register</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
 }
 
 //Users
@@ -154,6 +216,7 @@ class UserScreen extends React.Component {
     };
     this.touchUser = this.touchUser.bind(this);
     this.messages = this.messages.bind(this);
+    this.sendLocation = this.sendLocation.bind(this);
   }
 
   componentDidMount(){
@@ -211,6 +274,57 @@ class UserScreen extends React.Component {
     this.props.navigation.navigate('Messages')
   }
 
+  sendLocation = async(user) => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      //handle failure
+      console.log('oh no!', status);
+      return;
+    }
+    let location = await Location.getCurrentPositionAsync({enableHighAccuracy: true});
+    // console.log(location);
+    fetch('https://hohoho-backend.herokuapp.com/messages', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        to: user._id,
+        location: {
+          longitude: location.coords.longitude,
+          latitude: location.coords.latitude,
+        }
+      })
+    })
+    .then((res) => {
+      // console.log(res);
+      return res.json();
+    })
+    // .then(res => res.json())
+    .then((resJson) => {
+      if (resJson.success){
+        // console.log(resJson);
+        Alert.alert(
+          'Smacked!',
+          'Location and smack successfully dished to dat ' + user.username + '!',
+          [{text: 'Squaaa'}]
+        );
+        this.props.navigation.navigate('Messages');
+        // this.messages();
+      } else {
+        Alert.alert(
+          'Failure!',
+          'Cannot share location wit dat ' + user.username + '.',
+          [{text: 'Sad.'}]
+        );
+      }
+      // console.log('message sent to ' + user.username);
+    })
+    .catch((err2) => {
+      console.log('error with location share!', err2);
+    })
+  }
+
   static navigationOptions = ({ navigation }) => ({
     title: 'Who you wanna smack?',
     headerRight: <Button title='Receipts' onPress={() => {navigation.state.params.onRightPress()}}/>
@@ -221,184 +335,245 @@ class UserScreen extends React.Component {
       <View style={styles.hoBox}>
         <ListView
           dataSource={this.state.dataSource}
-          renderRow={(rowData) => <TouchableOpacity onPress={() => this.touchUser(rowData)} style={styles.eachHo}><Text style={styles.hoText}>{rowData.username}</Text></TouchableOpacity>}
-        />
-      </View>
-    )
-  };
-}
-
-//messages
-class MessageScreen extends React.Component {
-
-  constructor(props){
-    super(props);
-    const ds2 = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    this.state = {
-      dataSource2: ds2.cloneWithRows([
-        'Moose', 'Corey', 'Allie', 'Jay', 'Graham', 'Darwish', 'Abhi Fitness'
-      ])
-    };
-    // this.touchUser = this.touchUser.bind(this);
-  }
-
-  componentDidMount(){
-    fetch('https://hohoho-backend.herokuapp.com/messages?username'+this.props.navigation.state.params.username
-    +'&password=' +this.props.navigation.state.params.password, {
-      method: 'GET',
-      headers: {
-        "Content-Type": "application/json"
-      },
-    })
-    .then((response2) => response2.json())
-    .then((resp2Json) => {
-      let ds2 = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-      this.setState({
-        dataSource2: ds2.cloneWithRows(resp2Json.messages)
-      });
-      console.log('messages', this.state.dataSource2);
-    })
-    .catch((err) => {
-      console.log('error', err);
-    });
-  }
-
-  static navigationOptions = {
-    title: 'Smack Receipts'
-  };
-
-  render(){
-    return(
-      <View style={styles.hoMessageBox}>
-        <ListView
-          dataSource={this.state.dataSource2}
           renderRow={(rowData) =>
-            <View style={styles.eachHoMessage}>
-              <Text style={styles.hoMessageText}>From: {rowData}</Text>
-          </View>}
-        />
-      </View>
-    )
-  };
-}
+            <TouchableOpacity
+              onPress={() => this.touchUser(rowData)}
+              onLongPress={() => this.sendLocation(rowData)}
+              delayLongPress={1000}
+              style={styles.eachHo}>
+              <View>
+                <Text style={styles.hoText}>{rowData.username}</Text>
+              </View>
+            </TouchableOpacity>}
+          />
+        </View>
+      )
+    };
+  }
+
+  //messages
+  class MessageScreen extends React.Component {
+
+    constructor(props){
+      super(props);
+      const ds2 = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+      this.state = {
+        dataSource2: ds2.cloneWithRows([])
+      };
+      // this.touchUser = this.touchUser.bind(this);
+      this.timeClean = this.timeClean.bind(this);
+    }
+
+    componentDidMount(){
+      // console.log('mounting smack');
+      fetch('https://hohoho-backend.herokuapp.com/messages', {
+        method: 'GET',
+        headers: {
+          "Content-Type": "application/json"
+        },
+      })
+      .then((response2) => response2.json())
+      .then((resp2Json) => {
+        // console.log('hello', resp2Json);
+        let ds2 = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        this.setState({
+          dataSource2: ds2.cloneWithRows(resp2Json.messages)
+        });
+        // console.log('messages', this.state.dataSource2);
+      })
+      .catch((err) => {
+        console.log('error', err);
+      });
+    }
+
+    static navigationOptions = {
+      title: 'Smack Receipts'
+    };
+
+    timeClean(time){
+      const bad = new Date(time);
+      const clean = bad.toDateString();
+      return clean;
+    }
+
+    render(){
+
+      // console.log('x');
+      return(
+        <View style={styles.hoMessageBox}>
+          <ListView
+            dataSource={this.state.dataSource2}
+            renderRow={(rowData) =>
+              <View style={styles.eachHoMessage}>
+                <Text style={styles.hoMessageText}>From: {rowData.from.username}</Text>
+                <Text style={styles.hoMessageText}>To: {rowData.to.username}</Text>
+                <Text style={styles.hoMessageText}>At {this.timeClean(rowData.timestamp)}</Text>
+                {/* {console.log('location', rowData.location)} */}
+                {(rowData.location && rowData.location.longitude) ?
+                  <MapView
+                    style={styles.hoLocation}
+                    showsUserLocation={true}
+                    scrollEnabled={false}
+                    region={{
+                      longitude: rowData.location.longitude,
+                      latitude: rowData.location.latitude,
+                      longitudeDelta: 1,
+                      latitudeDelta: 1
+                    }}
+                  />
+                  :
+                  <Text>No Location received</Text>
+                }
+              </View>}
+            />
+          </View>
+        )
+      };
+    }
 
 
-//Navigator
-export default StackNavigator({
-  Login: {
-    screen: LoginScreen,
-  },
-  Register: {
-    screen: RegisterScreen,
-  },
-  Users: {
-    screen: UserScreen
-  },
-  Messages: {
-    screen: MessageScreen
-  },
-}, {initialRouteName: 'Login'});
+    //Navigator
+    export default StackNavigator({
+      Login: {
+        screen: LoginScreen,
+      },
+      Register: {
+        screen: RegisterScreen,
+      },
+      Users: {
+        screen: UserScreen
+      },
+      Messages: {
+        screen: MessageScreen
+      },
+    }, {initialRouteName: 'Login'});
 
 
-//Styles
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#96DC78',
-  },
-  containerFull: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'stretch',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  textBig: {
-    fontSize: 36,
-    textAlign: 'center',
-    margin: 10,
-  },
-  button: {
-    alignSelf: 'stretch',
-    paddingTop: 10,
-    paddingBottom: 10,
-    marginTop: 10,
-    marginLeft: 5,
-    marginRight: 5,
-    borderRadius: 5
-  },
-  buttonRed: {
-    backgroundColor: '#FF585B',
-  },
-  buttonWhite: {
-    backgroundColor: 'white',
-    // color: '#1E6400'
-  },
-  buttonGreen: {
-    backgroundColor: '#1E6400',
-    // color: 'white'
-  },
-  buttonLabel: {
-    textAlign: 'center',
-    fontSize: 16,
-    color: 'white'
-  },
-  buttonLabelGreen: {
-    textAlign: 'center',
-    fontSize: 16,
-    color: '#1E6400'
-  },
-  register: {
-    height: 40,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white',
-    borderRadius: 2,
-    marginRight: 30,
-    marginLeft: 30,
-    padding: 5
-  },
-  hoBox: {
-    flex: 1,
-    // alignItems: 'center',
-    // justifyContent: 'center'
-  },
-  eachHo: {
-    flex: 1,
-    // height: 100,
-    paddingTop: 10,
-    paddingBottom: 10,
-    backgroundColor: '#6BBC48'
-  },
-  hoText: {
-    textAlign: 'center',
-    fontSize: 24
-  },
-  hoMessageBox: {
-    flex: 1,
-    // alignItems: 'center',
-    // justifyContent: 'center'
-  },
-  eachHoMessage: {
-    flex: 1,
-    // height: 100,
-    paddingTop: 10,
-    paddingBottom: 10,
-    backgroundColor: '#6BBC48'
-  },
-  hoMessageText: {
-    // textAlign: 'center',
-    fontSize: 24
-  },
-});
+    //Styles
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#FFDB80',
+      },
+      containerFull: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'stretch',
+        backgroundColor: '#FFDB80',
+      },
+      welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+      },
+      instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+      },
+      textBig: {
+        fontSize: 30,
+        textAlign: 'center',
+        margin: 10,
+        color: '#3e0040'
+      },
+      button: {
+        alignSelf: 'stretch',
+        paddingTop: 10,
+        paddingBottom: 10,
+        marginTop: 10,
+        marginLeft: 5,
+        marginRight: 5,
+        borderRadius: 5
+      },
+      buttonRed: {
+        backgroundColor: '#FF585B',
+      },
+      buttonWhite: {
+        backgroundColor: 'white',
+        // color: '#1E6400'
+      },
+      buttonPurple: {
+        backgroundColor: '#3e0040',
+        // color: 'white'
+      },
+      buttonLabel: {
+        textAlign: 'center',
+        fontSize: 16,
+        color: 'white'
+      },
+      buttonLabelPurple: {
+        textAlign: 'center',
+        fontSize: 16,
+        color: '#3e0040'
+      },
+      register: {
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+        borderRadius: 2,
+        marginRight: 30,
+        marginLeft: 30,
+        padding: 5
+      },
+      hoBox: {
+        flex: 1,
+        // alignItems: 'center',
+        // justifyContent: 'center',
+        backgroundColor: '#FFDE8E',
+      },
+      eachHo: {
+        flex: 1,
+        height: 50,
+        paddingTop: 10,
+        paddingBottom: 10,
+        marginTop: 2,
+        marginBottom: 2,
+        backgroundColor: '#FFEAB6',
+        borderWidth: 2,
+        borderRadius: 5,
+        borderColor: '#FFF7E4'
+      },
+      hoText: {
+        textAlign: 'center',
+        fontSize: 24
+      },
+      hoMessageBox: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#FFF7E4',
+
+      },
+      eachHoMessage: {
+        // flex: 1,
+        // height: 100,
+        paddingTop: 15,
+        paddingBottom: 15,
+        paddingLeft: 15,
+        paddingRight: 15,
+        backgroundColor: '#FFEAB6',
+        borderColor: '#D3A200',
+        borderWidth: 3,
+        borderRadius: 5,
+        marginTop: 7,
+      },
+      hoMessageText: {
+        // textAlign: 'center',
+        fontSize: 12
+      },
+      hoLocation: {
+        // flex: 2,
+        height: 150,
+        width: 150,
+        borderRadius: 80,
+        margin: 5,
+      },
+      logo: {
+        height: 200,
+        width: 200,
+      }
+    });
--- day2/maps/App.js
+++ day2/maps/App.js
@@ -3,7 +3,8 @@ import {
   TouchableOpacity,
   StyleSheet,
   Text,
-  View
+  View,
+  AsyncStorage
  } from 'react-native';
 
 import {
@@ -11,6 +12,42 @@ import {
 } from 'expo';
 
 class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      region: {
+        latitude: 37.257725,
+        longitude: 23.152210,
+        latitudeDelta: 0.05,
+        longitudeDelta: 0.05,
+      }
+    }
+    // this.explore=this.explore.bind(this);
+    this.onRegionChange = this.onRegionChange.bind(this);
+  }
+
+  // explore(lat, long){
+  //   this.setState({
+  //     lat: lat,
+  //     long: long
+  //   });
+  // }
+
+  onRegionChange(region){
+    AsyncStorage.setItem('location', JSON.stringify(region))
+    .then(() => {
+      this.setState({ region });
+    })
+  }
+
+  componentDidMount(){
+    AsyncStorage.getItem('location')
+    .then((region) => {
+      this.setState({ region: JSON.parse(region) });
+    });
+  }
+
   render() {
     return (
       <View style={{
@@ -18,31 +55,82 @@ class App extends React.Component {
         }}>
         <View style={{flex: 1, flexDirection: 'row'}}>
           <TouchableOpacity
-            style={{flex: 1,
-              borderWidth: 1,
-              alignItems: 'center',
-              justifyContent: 'center'}}>
-            <Text>Istanbul</Text>
+            style={styles.eachNav}
+            onPress={() => {
+              this.setState({
+                region: {
+                  latitude: 51.521627,
+                  longitude: -0.157542,
+                  latitudeDelta: 0.005,
+                  longitudeDelta: 0.005,
+                }
+              })
+            }}>
+            <Text>London</Text>
           </TouchableOpacity>
           <TouchableOpacity
-            style={{flex: 1,
-              borderWidth: 1,
-              alignItems: 'center',
-              justifyContent: 'center'}}>
-            <Text>Sydney</Text>
+            style={styles.eachNav}
+            onPress={() => this.setState({
+              region: {
+                latitude: 6.468474,
+                longitude: 3.545385,
+                latitudeDelta: 0.005,
+                longitudeDelta: 0.005,
+            }
+            })}>
+            <Text>Home</Text>
           </TouchableOpacity>
           <TouchableOpacity
-            style={{flex: 1,
-              borderWidth: 1,
-              alignItems: 'center',
-              justifyContent: 'center'}}>
-            <Text>Hong Kong</Text>
+            style={styles.eachNav}
+            onPress={() => this.setState({
+              region: {
+                latitude: 42.269673,
+                longitude: -71.342741,
+                latitudeDelta: 0.005,
+                longitudeDelta: 0.005,
+              }
+            })}>
+            <Text>Natick</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.eachNav}
+            onPress={() => {
+              navigator.geolocation.getCurrentPosition(
+                (success) => {
+                  this.setState({
+                    region: {
+                      latitude: success.coords.latitude,
+                      longitude: success.coords.longitude,
+                      latitudeDelta: 0.005,
+                      longitudeDelta: 0.005,
+                    }
+                  })
+                },
+                (error) => {
+
+                },
+                {}
+              )
+            }}>
+            <Text>Here</Text>
           </TouchableOpacity>
         </View>
-        <MapView style={{flex: 7}} />
+        <MapView style={{flex: 7}}
+          region={this.state.region}
+          onRegionChangeComplete={this.onRegionChange}
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  eachNav : {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    paddingBottom: 20
+  }
+})
 
 export default App;
