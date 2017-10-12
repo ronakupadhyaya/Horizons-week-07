b73f15a2b24f4035f77233230bdb8c9f0672741f
jeff <tangj1122@gmail.com>
2017-10-12 15:16:34 -0700

    fin part3, dynamic users

--- day1/hohoho/App.js
+++ day1/hohoho/App.js
@@ -10,16 +10,16 @@ import {
   Button
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
-
+const baseUrl = 'https://hohoho-backend.herokuapp.com/'
 
 //Screens
-class LoginScreen extends React.Component {
+class HomeScreen extends React.Component {
   static navigationOptions = {
-    title: 'Login'
+    title: 'Home'
   };
 
   press() {
-
+    this.props.navigation.navigate('Login');
   }
   register() {
     this.props.navigation.navigate('Register');
@@ -29,7 +29,7 @@ class LoginScreen extends React.Component {
     return (
       <View style={styles.container}>
         <Text style={styles.textBig}>Login to HoHoHo!</Text>
-        <TouchableOpacity onPress={ () => {this.press()} } style={[styles.button, styles.buttonGreen]}>
+        <TouchableOpacity style={[styles.button, styles.buttonGreen]} onPress={ () => {this.press()} }>
           <Text style={styles.buttonLabel}>Tap to Login</Text>
         </TouchableOpacity>
         <TouchableOpacity style={[styles.button, styles.buttonBlue]} onPress={ () => {this.register()} }>
@@ -44,30 +44,172 @@ class RegisterScreen extends React.Component {
   static navigationOptions = {
     title: 'Register'
   };
-
+  postRegister() {
+    fetch(baseUrl + 'register', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password,
+      })
+    })
+    .then(response => response.json())
+    .then(responseJson => {
+      console.log(responseJson);
+      this.props.navigation.goBack();
+    })
+    .catch(err => {
+      console.log('400:', err);
+    })
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.textBig}>Register</Text>
+        <TextInput
+          style={styles.inputBox}
+          placeholder="Enter your username"
+          onChangeText={(text) => this.setState({username: text})}
+        />
+        <TextInput
+          style={styles.inputBox}
+          placeholder="Enter your password"
+          onChangeText={(text) => this.setState({password: text})}
+        />
+        <TouchableOpacity style={[styles.button, styles.buttonBlue]} onPress={ () => {this.postRegister()} }>
+          <Text style={styles.buttonLabel}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    )
+  }
+}
+          /*secureTextEntry={true}*/
+
+class LoginScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Login'
+  };
+  postLogin() {
+    fetch(baseUrl + 'login', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password,
+      })
+    })
+    .then(response => response.json())
+    .then(responseJson => {
+      console.log(responseJson);
+      if (responseJson.success) {
+        this.props.navigation.navigate('Users');
+      } else {
+        this.props.navigation.navigate('Home');
+        alert('401: Incorrect credentials');
+      }
+    })
+    .catch(err => {
+      alert('400: Bad user input');
+    })
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.inputBox}
+          placeholder="Username"
+          onChangeText={(text) => this.setState({username: text})}
+        />
+        <TextInput
+          style={styles.inputBox}
+          placeholder="Password"
+          onChangeText={(text) => this.setState({password: text})}
+        />
+        <TouchableOpacity style={[styles.button, styles.buttonGreen]} onPress={ () => {this.postLogin()} }>
+          <Text style={styles.buttonLabel}>Login</Text>
+        </TouchableOpacity>
       </View>
     )
   }
 }
 
+          /*secureTextEntry={true} TODO*/
+
+class UsersScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Users'
+  };
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    /* this.state = { */
+    /*   dataSource: ds.cloneWithRows([ */
+    /*     'Moose', 'Corey', 'Allie', 'Jay', 'Graham', 'Darwish', 'Abhi Fitness' */
+    /*   ]) */
+    /* }; */
+    fetch(baseUrl + 'users', {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json"
+      },
+    })
+    .then(response => response.json())
+    .then(responseJson => {
+      this.setState({
+        dataSource: ds.cloneWithRows(responseJson.users)
+      });
+    })
+    .catch(err => {
+      alert('Error: ' + err);
+      console.log('Error: ' + err);
+    })
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <ListView
+          renderRow={item => {
+            return (
+              <View key={item._id}><Text>{item.username}</Text></View>
+            )
+          }}
+          dataSource={this.state.dataSource}
+        />
+      </View>
+    );
+  }
+}
 
 //Navigator
 export default StackNavigator({
-  Login: {
-    screen: LoginScreen,
+  Home: {
+    screen: HomeScreen,
   },
   Register: {
     screen: RegisterScreen,
   },
-}, {initialRouteName: 'Login'});
+  Login: {
+    screen: LoginScreen,
+  },
+  Users: {
+    screen: UsersScreen,
+  },
+}, {initialRouteName: 'Home'});
 
 
 //Styles
 const styles = StyleSheet.create({
+  inputBox: {
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'grey',
+    marginBottom: 5,
+    marginLeft: 5,
+    marginRight: 5,
+  },
   container: {
     flex: 1,
     justifyContent: 'center',
