be77609cc0c796947bb22a218b18444ddd878540
crawforde <crawforde@kenyon.edu>
2017-10-12 17:57:33 -0700

    Parts 1 and 2

--- day1/hohoho/App.js
+++ day1/hohoho/App.js
@@ -6,20 +6,20 @@ import {
   TouchableOpacity,
   TextInput,
   ListView,
-  Alert,
-  Button
+  Alert
 } from 'react-native';
+import Button from 'apsl-react-native-button';
 import { StackNavigator } from 'react-navigation';
-
+const SERVER_URL = "https://hohoho-backend.herokuapp.com";
 
 //Screens
-class LoginScreen extends React.Component {
+class HomeScreen extends React.Component {
   static navigationOptions = {
     title: 'Login'
   };
 
   press() {
-
+    this.props.navigation.navigate('Login');
   }
   register() {
     this.props.navigation.navigate('Register');
@@ -40,37 +40,145 @@ class LoginScreen extends React.Component {
   }
 }
 
-class RegisterScreen extends React.Component {
+
+class LoginScreen extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      username: '',
+      password: ''
+    }
+  }
+
   static navigationOptions = {
-    title: 'Register'
+    title: 'Login'
   };
+
+  handleLogin(){
+    fetch(`${SERVER_URL}/login`,{
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password
+      })
+    })
+    .then((resp)=>
+      resp.json()
+    ).then((respJson)=>{
+      alert('Success! ' + respJson.user.username + ' successfully logged in.');
+      this.props.navigation.navigate('Home');
+    }).catch((err)=>{
+      alert('Error: login failed.');
+    });
+  }
 
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.textBig}>Register</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={(username) => this.setState({username})}
+          value={this.state.username}
+          placeholder="Username"
+        ></TextInput>
+        <TextInput
+          secureTextEntry={true}
+          style={styles.textInput}
+          onChangeText={(password) => this.setState({password})}
+          value={this.state.password}
+          placeholder="Password"
+        ></TextInput>
+        <TouchableOpacity onPress={()=>this.handleLogin()} style={styles.button}>
+          <Text style={{fontSize: 20, color: 'white'}}>
+            Login
+          </Text>
+        </TouchableOpacity>
       </View>
-    )
+    );
   }
 }
 
+class RegisterScreen extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      username: '',
+      password: ''
+    }
+  }
+
+  static navigationOptions = {
+    title: 'Register'
+  };
+
+  handleRegister(){
+    fetch(`${SERVER_URL}/register`,{
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password
+      })
+    })
+    .then((resp)=>
+      resp.json()
+    ).then((respJson)=>{
+      alert('Success! Account created for ' + respJson.user.username + '.');
+      this.props.navigation.navigate('Home');
+    }).catch((err)=>{
+      alert('Error: account not registerd.');
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={(username) => this.setState({username})}
+          value={this.state.username}
+          placeholder="Username"
+        ></TextInput>
+        <TextInput
+          secureTextEntry={true}
+          style={styles.textInput}
+          onChangeText={(password) => this.setState({password})}
+          value={this.state.password}
+          placeholder="Password"
+        ></TextInput>
+        <TouchableOpacity onPress={()=>this.handleRegister()} style={styles.button}>
+          <Text style={{fontSize: 20, color: 'white'}}>
+            Register
+          </Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
 
 //Navigator
 export default StackNavigator({
+  Home: {
+    screen: HomeScreen,
+  },
   Login: {
     screen: LoginScreen,
   },
   Register: {
     screen: RegisterScreen,
   },
-}, {initialRouteName: 'Login'});
+}, {initialRouteName: 'Home'});
 
 
 //Styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
@@ -95,6 +203,11 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
+  textInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
   button: {
     alignSelf: 'stretch',
     paddingTop: 10,
@@ -102,7 +215,10 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginLeft: 5,
     marginRight: 5,
-    borderRadius: 5
+    borderRadius: 5,
+    backgroundColor: 'red',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   buttonRed: {
     backgroundColor: '#FF585B',
