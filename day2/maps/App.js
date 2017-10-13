0fbdaefd7912353213e58e9a322ad6ca8e491ccc
natedana <natepdana@gmail.com>
2017-10-13 10:28:27 -0700

    w7d4

--- day2/maps/App.js
+++ day2/maps/App.js
@@ -3,7 +3,8 @@ import {
   TouchableOpacity,
   StyleSheet,
   Text,
-  View
+  View,
+  AsyncStorage
  } from 'react-native';
 
 import {
@@ -11,6 +12,53 @@ import {
 } from 'expo';
 
 class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      lat: 40,
+      lon: -81,
+      latD: 0.05,
+      lonD: 0.025
+    }
+  }
+
+  componentDidMount() {
+    console.log("DID MOUNT CALLED");
+    AsyncStorage.getItem('region')
+      .then((region) => {
+        console.log("MOUNT", region);
+        region = JSON.parse(region)
+        this.setState({
+          lat: region.latitude,
+          lon: region.longitude,
+          latD: region.latitudeDelta,
+          lonD: region.longitudeDelta
+        })
+      })
+  }
+  changeLoc(order) {
+    const latList = [41.067841,-33.866174,22.294074]
+    const lonList = [29.045258,151.220345,114.171995]
+    this.setState({
+      lat: latList[order],
+      lon: lonList[order]
+    })
+  }
+  myLoc() {
+    navigator.geolocation.getCurrentPosition(
+      (success)=>{
+        console.log('SPOT',success.coords.latitude,',',success.coords.longitude)
+        this.setState({
+          lat: success.coords.latitude,
+          lon: success.coords.longitude
+        });
+      },
+      (error)=>{
+        alert(error)
+      },
+      {}
+    )
+  }
   render() {
     return (
       <View style={{
@@ -21,25 +69,40 @@ class App extends React.Component {
             style={{flex: 1,
               borderWidth: 1,
               alignItems: 'center',
-              justifyContent: 'center'}}>
+              justifyContent: 'center'}} onPress={()=>this.changeLoc(0)}>
             <Text>Istanbul</Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={{flex: 1,
               borderWidth: 1,
               alignItems: 'center',
-              justifyContent: 'center'}}>
+              justifyContent: 'center'}} onPress={()=>this.changeLoc(1)}>
             <Text>Sydney</Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={{flex: 1,
               borderWidth: 1,
               alignItems: 'center',
-              justifyContent: 'center'}}>
+              justifyContent: 'center'}} onPress={()=>this.changeLoc(2)}>
             <Text>Hong Kong</Text>
           </TouchableOpacity>
+          <TouchableOpacity
+            style={{flex: 1,
+              borderWidth: 1,
+              alignItems: 'center',
+              justifyContent: 'center'}} onPress={()=>this.myLoc()}>
+            <Text>My Location</Text>
+          </TouchableOpacity>
         </View>
-        <MapView style={{flex: 7}} />
+        <MapView style={{flex: 7}} region={{
+          latitude: this.state.lat,
+          longitude: this.state.lon,
+          latitudeDelta: this.state.latD,
+          longitudeDelta: this.state.lonD,
+        }} onRegionChangeComplete={(region)=>{
+          console.log("REG CHANGE COMP",region);
+          AsyncStorage.setItem('region',JSON.stringify(region));
+        }}/>
       </View>
     );
   }
