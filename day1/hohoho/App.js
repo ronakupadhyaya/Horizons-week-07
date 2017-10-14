8098327115ff768292375cf5bae9180676a8739d
jeff <tangj1122@gmail.com>
2017-10-13 21:18:36 -0700

    can't get process.env variables ...

--- day1/hohoho/App.js
+++ day1/hohoho/App.js
@@ -10,14 +10,15 @@ import MessagesScreen from './Components/MessagesScreen';
 import TransactionsScreen from './Components/TransactionsScreen';
 /* const baseUrl = 'https://hohoho-backend.herokuapp.com/' */
 
-        /* dot=<View style={{backgroundColor:'rgba(0,0,0,.2)', width: 8, height: 8,borderRadius: 4, marginLeft: 3, marginRight: 3, marginTop: 3, marginBottom: 3,}} /> */
+
+
 class SwiperScreen extends React.Component {
   /* static navigationOptions = { */
   /*   title: 'HoHoHo!' */
   /* }; */
   render() {
 
-    const dotElem = <View style={{backgroundColor:'rgba(0,0,0,.2)', width: 8, height: 8,borderRadius: 4, marginLeft: 3, marginRight: 3, marginTop: 3, marginBottom: 3,}} />
+    const dotElem = <View style={{backgroundColor:'rgba(0,0,0,.2)', width: 8, height: 8,borderRadius: 4, marginLeft: 3, marginRight: 3, marginTop: 3, marginBottom: 3,}} />;
 
     return (
       <Swiper
--- day1/hohoho/env.sh
+++
@@ -1,1 +0,0 @@
-export BASE_URL=https://hohoho-backend.herokuapp.com/
