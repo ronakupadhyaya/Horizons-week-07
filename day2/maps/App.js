1e451d3fcf7efce6d60936d7cb69d2d5b2cdf6e7
Kobie <spaet062@umn.edu>
2017-10-17 09:07:46 -0700

    merge w horizons master'

--- day2/maps/App.js
+++ day2/maps/App.js
@@ -3,14 +3,53 @@ import {
   TouchableOpacity,
   StyleSheet,
   Text,
-  View
- } from 'react-native';
+  View,
+  AsyncStorage,
+} from 'react-native';
+
+import {
+  StackNavigator,
+} from 'react-navigation';
 
 import {
   MapView
 } from 'expo';
 
-class App extends React.Component {
+console.disableYellowBox = true;
+
+class MapScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      region: {
+        latitude: 50,
+        longitude: 50,
+        latitudeDelta: .05,
+        longitudeDelta: .05,
+      },
+    }
+  }
+
+  static navigationOptions = {
+    title: 'Maps'
+  };
+
+  componentDidMount() {
+    AsyncStorage.getItem('location')
+    .then( location => {
+      this.setState({
+        region: JSON.parse(location).region
+      })
+    })
+    .catch( error => {console.log('error in async storage mount: ', error);})
+  }
+
+  onRegionChangeComplete(region) {
+    AsyncStorage.setItem('location', JSON.stringify({region}))
+    .catch( error => {console.log('error in on region change: ', error);})
+  }
+
   render() {
     return (
       <View style={{
@@ -21,28 +60,96 @@ class App extends React.Component {
             style={{flex: 1,
               borderWidth: 1,
               alignItems: 'center',
-              justifyContent: 'center'}}>
+              justifyContent: 'center'}}
+            onPress={ () =>
+              this.setState({
+                region: {
+                  latitude: 41.067841,
+                  longitude: 29.045258,
+                  latitudeDelta: this.state.region.latitudeDelta,
+                  longitudeDelta: this.state.region.longitudeDelta,
+                }
+              })
+            }
+            >
             <Text>Istanbul</Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={{flex: 1,
               borderWidth: 1,
               alignItems: 'center',
-              justifyContent: 'center'}}>
+              justifyContent: 'center'}}
+            onPress={ () =>
+              this.setState({
+                region: {
+                  latitude: 22.294074,
+                  longitude: 114.171995,
+                  latitudeDelta: this.state.region.latitudeDelta,
+                  longitudeDelta: this.state.region.longitudeDelta,
+                }
+              })
+            }
+            >
+            <Text>Hong Kong</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{flex: 1,
+              borderWidth: 1,
+              alignItems: 'center',
+              justifyContent: 'center'}}
+            onPress={ () =>
+              this.setState({
+                region: {
+                  latitude: -33.866174,
+                  longitude: 151.220345,
+                  latitudeDelta: this.state.region.latitudeDelta,
+                  longitudeDelta: this.state.region.longitudeDelta,
+                }
+              })
+            }
+            >
             <Text>Sydney</Text>
           </TouchableOpacity>
           <TouchableOpacity
             style={{flex: 1,
               borderWidth: 1,
               alignItems: 'center',
-              justifyContent: 'center'}}>
-            <Text>Hong Kong</Text>
+              justifyContent: 'center'}}
+            onPress={ () =>
+              navigator.geolocation.getCurrentPosition(
+                success => {
+                  this.setState({
+                    region: {
+                      latitude: success.coords.latitude,
+                      longitude: success.coords.longitude,
+                      longitudeDelta: success.coords.longitudeDelta,
+                      latitudeDelta: success.coords.latitudeDelta,
+                    }
+                  })
+                },
+                error => {
+                  console.log("error: ", error);
+                },
+                {}
+              )
+            }
+            >
+            <Text>Here</Text>
           </TouchableOpacity>
         </View>
-        <MapView style={{flex: 7}} />
+        <MapView
+          style={{flex: 8}}
+          region={this.state.region}
+          showsUserLocation={true}
+          onRegionChangeComplete={ (region) => this.onRegionChangeComplete(region) }
+        />
       </View>
     );
   }
 }
 
-export default App;
+export default StackNavigator({
+  Map: {
+    screen: MapScreen,
+  },
+}, {initialRouteName: 'Map'});
